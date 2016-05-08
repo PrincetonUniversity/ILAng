4d883170e6765c224ff08ea48af7599ceb6bd7cf
Bo-Yuan Huang <byhuang@princeton.edu>
2016-05-08 14:45:47 -0400

    Add uinst for SHA

--- models/SHA/simSHAuinst.py
+++ models/SHA/simSHAuinst.py
@@ -1,6 +1,7 @@
-# A simple simulator for SHA+XRAM
+# A simple simulator for SHA+XRAM (uinst version)
 
 from mmio import mmiodev, NOP, RD, WR
+import ila
 import sha as SHAFunc
 
 def as_chars(s, n):
@@ -23,27 +24,40 @@ class SHA(mmiodev):
     SHA_OP2  = 3
     SHA_WR   = 4
 
+    ADDR_START  = 0xfe00
+    ADDR_STATE  = 0xfe01
+    ADDR_RDADDR = 0xfe02
+    ADDR_WRADDR = 0xfe04
+    ADDR_LEN    = 0xfe06
+
     def __init__(self):
         mmiodev.__init__(self)
-        self.addReg('sha_start', 0xfe00, 1)
-        self.addReg('sha_state', 0xfe01, 1)
-        self.addReg('sha_rdaddr', 0xfe02, 2)
-        self.addReg('sha_wraddr', 0xfe04, 2)
-        self.addReg('sha_len', 0xfe06, 2)
+        self.addReg('sha_start', self.ADDR_START, 1, readonly=True)
+        self.addReg('sha_state', self.ADDR_STATE, 1, readonly=True)
+        self.addReg('sha_rdaddr', self.ADDR_RDADDR, 2)
+        self.addReg('sha_wraddr', self.ADDR_WRADDR, 2)
+        self.addReg('sha_len', self.ADDR_LEN, 2)
 
         self.bytes_read = 0
-        self.rd_data  = [0] * 64
-        self.hs_data  = [0] * 20
+        self.rd_data = [0] * 64
+        self.hs_data = [0] * 20
+        self.xram = mmiodev
 
         self.sha = SHAFunc.new()
-
-        # Create easy access properties.
+       
+    # create easy access properties
     sha_state  = property(lambda s: s.getRegI('sha_state'), lambda s, v: s.setRegI('sha_state', v))
     sha_rdaddr = property(lambda s: s.getRegI('sha_rdaddr'), lambda s, v: s.setRegI('sha_rdaddr', v))
     sha_wraddr = property(lambda s: s.getRegI('sha_wraddr'), lambda s, v: s.setRegI('sha_wraddr', v))
     sha_len    = property(lambda s: s.getRegI('sha_len'), lambda s, v: s.setRegI('sha_len', v))
 
-    def simulate(self, s_in):
+    def get(self, s, name, default):
+        if name in s:
+            return s[name]
+        else:
+            return default
+
+    def extract(self, s_in):
         cmd     = s_in['cmd']
         cmdaddr = s_in['cmdaddr']
         cmddata = s_in['cmddata']
@@ -52,30 +66,37 @@ class SHA(mmiodev):
         self.sha_rdaddr = s_in['sha_rdaddr']
         self.sha_wraddr = s_in['sha_wraddr']
         self.sha_len    = s_in['sha_len']
-        self.bytes_read = s_in['sha_bytes_read']
-        self.rd_data    = s_in['sha_rd_data']
-        self.hs_data    = s_in['sha_hs_data']
-        self.xram       = s_in['XRAM']
+        self.bytes_read = self.get(s_in, 'sha_bytes_read', 0)
+        self.rd_data    = self.get(s_in, 'sha_rd_data', 0)
+        self.hs_data    = self.get(s_in, 'sha_hs_data', 0)
+        self.xram       = self.get(s_in, 'XRAM', ila.MemValues(16, 8, 0x0))
 
-        # default dataout.
-        dataout = 0;
-        # execute command.
-        started = False
+        return cmd, cmdaddr, cmddata
+
+    def simMacro(self, s_in):
+        cmd, cmdaddr, cmddata = self.extract(s_in)
+
+        # default dataout
+        dataout = 0
+        # execute command
         if cmd == RD:
             found, data = self.read(cmdaddr)
             if found:
                 dataout = data
         elif cmd == WR and self.sha_state == self.SHA_IDLE:
-            if cmdaddr == 0xfe00:
+            if cmdaddr == self.ADDR_START:
                 if cmddata == 1:
                     self.sha_state = self.SHA_RD
                     self.bytes_read = 0
-                    started = True
             else:
                 self.write(cmdaddr, cmddata)
+        s_out = self.s_dict()
+        s_out['dataout'] = dataout
+        return s_out
 
-        # do the operations.
-        if not started and self.sha_state == self.SHA_RD:
+    def simMicro(self, s_in):
+        cmd, cmdaddr, cmddata = self.extract(s_in)
+        if self.sha_state == self.SHA_RD:
             self.rd_data = 0
             for i in xrange(64):
                 addr = (self.sha_rdaddr + self.bytes_read + 63 - i) & 0xffff
@@ -83,9 +104,9 @@ class SHA(mmiodev):
                 self.rd_data |= byte << (i*8)
             self.bytes_read = self.bytes_read + 64
             self.sha_state = self.SHA_OP1
-        elif not started and self.sha_state == self.SHA_OP1:
+        elif self.sha_state == self.SHA_OP1:
             self.sha_state = self.SHA_OP2
-        elif not started and self.sha_state == self.SHA_OP2:
+        elif self.sha_state == self.SHA_OP2:
             if self.bytes_read < self.sha_len: # Need more blk
                 self.sha_state = self.SHA_RD
             else:
@@ -94,15 +115,13 @@ class SHA(mmiodev):
             self.sha.update(bytes_in)
             res = self.sha.digest()
             self.hs_data = to_num(res, 20)
-        elif not started and self.sha_state == self.SHA_WR:
+        elif self.sha_state == self.SHA_WR:
             for i in xrange(20):
                 addr = (self.sha_wraddr + 19 - i) & 0xffff
                 byte = (self.hs_data >> (i*8)) & 0xff
                 self.xram[addr] = byte
             self.sha_state = self.SHA_IDLE
-
         s_out = self.s_dict()
-        s_out['dataout'] = dataout
         return s_out
 
     def s_dict(self):
@@ -116,7 +135,7 @@ class SHA(mmiodev):
             'sha_hs_data'   : self.hs_data,
             'XRAM'          : self.xram
         }
-
+            
 def testSHA():
     sha = SHA()
     sha.sha_state = 0
@@ -126,15 +145,16 @@ def testSHA():
     s_in['cmd'] = WR
     s_in['cmdaddr'] = 0xfe06
     s_in['cmddata'] = 128
-    s_out = sha.simulate(s_in)
+    s_out = sha.simMacro(s_in)
     assert s_out['sha_len'] == 128
 
     s_in = sha.s_dict()
     s_in['cmd'] = RD
     s_in['cmdaddr'] = 0xfe06
     s_in['cmddata'] = 3
-    s_out = sha.simulate(s_in)
+    s_out = sha.simMacro(s_in)
     assert s_out['dataout'] == 128
+
 
 if __name__ == '__main__':
     testSHA()
--- models/SHA/synSHAuinst.py
+++ models/SHA/synSHAuinst.py
@@ -1,17 +1,17 @@
 import argparse
 import ila
 import time
-from simSHA import SHA
+from simSHAuinst import SHA
 
-def createSHAILA(synstates, enable_ps):
+def createSHAILA(enable_ps):
     m = ila.Abstraction("sha")
     m.enable_parameterized_synthesis = enable_ps
 
-    # I/O interface: this is where commands come from.
+    # I/O interface: this is where the commands come from
     cmd         = m.inp('cmd', 2)
     cmdaddr     = m.inp('cmdaddr', 16)
-    cmddata     = m.inp('cmddata', 8)
-    # response.
+    cmddata    = m.inp('cmddata', 8)
+    # response
     dataout     = m.reg('dataout', 8)
 
     # internal arch state.
@@ -21,9 +21,6 @@ def createSHAILA(synstates, enable_ps):
     oplen       = m.reg('sha_len', 16)
 
     # for the uinst.
-    bytes_read  = m.reg('sha_bytes_read', 16)
-    rd_data     = m.reg('sha_rd_data', 512)
-    hs_data     = m.reg('sha_hs_data', 160)
     xram        = m.mem('XRAM', 16, 8)
     sha         = m.fun('sha', 160, [512])
 
@@ -36,8 +33,8 @@ def createSHAILA(synstates, enable_ps):
               for addr in xrange(0xfe00, 0xfe10) for i in [0, 1, 2, 3, 4]]
     wrcmds = [(state == 0) & (cmd == 2) & (cmdaddr == addr)
               for addr in xrange(0xfe00, 0xfe10)]
-    nopcmds = [(state == i) & (cmd != 1) & (cmdaddr == addr)
-               for addr in xrange(0xfe00, 0xfe10) for i in [1, 2, 3, 4]]
+    nopcmds = [((state != 0) & (cmd != 1)) | ((state == 0) & (cmd != 1) & (cmd != 2))]
+
     m.decode_exprs = rdcmds + wrcmds + nopcmds
 
     # read commands.
@@ -60,56 +57,94 @@ def createSHAILA(synstates, enable_ps):
     mb_reg_wr('sha_wraddr', wraddr)
     mb_reg_wr('sha_len', oplen)
 
-    # state
-    state_next = ila.choice('state_next', [
-                    m.const(0, 3), m.const(1, 3), m.const(2, 3), 
-                    m.const(3, 3), m.const(4, 3),
-                    ila.ite(cmddata == 1, m.const(1, 3), state),
-                    ila.ite(bytes_read < oplen, m.const(1, 3), m.const(4, 3))])
-    m.set_next('sha_state', state_next)
-
     # these are for the uinst
-    # bytes_read
-    #bytes_read_inc = ila.ite(bytes_read+64 <= oplen, bytes_read+64, oplen)
-    bytes_read_inc = bytes_read + 64
-    bytes_read_rst = ila.ite(cmddata == 1, m.const(0, 16), bytes_read)
-    bytes_read_nxt = ila.choice('bytes_read_nxt', [
-            m.const(0, 16), bytes_read_inc, bytes_read_rst, bytes_read])
-    m.set_next('sha_bytes_read', bytes_read_nxt)
+    um = m.add_microabstraction('sha_compute', state != 0)
+    um.fetch_expr = state
+    um.decode_exprs = [state == i for i in [1, 2, 3, 4]]
+
+    # read data
+    rd_data     = um.reg('sha_rd_data', 512)
+    hs_data     = um.reg('sha_hs_data', 160)
+    bytes_read  = um.reg('sha_bytes_read', 16)
+    um.set_init('sha_bytes_read', um.const(0, 16))
+    uxram = m.getmem('XRAM')
+
+    usim = lambda s: SHA().simMicro(s)
+
+    # ustate
+    ustate      = um.getreg('sha_state')
+    ustate_nxt  = ila.choice('ustate_next', [
+                    m.const(0, 3), m.const(1, 3), m.const(2, 3), 
+                    m.const(3, 3), m.const(4, 3), 
+                    ila.ite(bytes_read < oplen, m.const(1, 3), m.const(4, 3))])
+    um.set_next('sha_state', ustate_nxt)
+
+    # bytes_read 
+    bytes_read_inc = bytes_read + 16
+    bytes_read_nxt = ila.choice('bytes_read_nxt', [bytes_read_inc, bytes_read])
+    um.set_next('sha_bytes_read', bytes_read_nxt)
+
     # rd_data
     rdblock_little = ila.loadblk(xram, rdaddr + bytes_read, 64)
     rdblock_big = ila.loadblk_big(xram, rdaddr + bytes_read, 64)
     rd_data_nxt = ila.choice('rd_data_nxt', rdblock_big, rdblock_little, rd_data)
-    m.set_next('sha_rd_data', rd_data_nxt)
+    um.set_next('sha_rd_data', rd_data_nxt)
+
     # hs_data
     sha_hs_data = ila.appfun(sha, [rd_data])
     hs_data_nxt = ila.choice('hs_data_nxt', sha_hs_data, hs_data)
-    m.set_next('sha_hs_data', hs_data_nxt)
+    um.set_next('sha_hs_data', hs_data_nxt)
+
     # xram write
     xram_w_sha_little = ila.storeblk(xram, wraddr, hs_data)
     xram_w_sha_big = ila.storeblk_big(xram, wraddr, hs_data)
     xram_nxt = ila.choice('xram_nxt', xram, xram_w_sha_little, xram_w_sha_big)
-    m.set_next('XRAM', xram_nxt)
+    um.set_next('XRAM', xram_nxt)
 
-    # synthesis.
-    sim = lambda s: SHA().simulate(s)
-    for s in synstates:
+    # micro-synthesis
+    st = time.clock()
+    t_elapsed = 0
+    for s in ['sha_state', 'sha_bytes_read', 'sha_rd_data', 'XRAM']:
+        print 'micro synthesis %s' % s
+        um.synthesize(s, usim)
+        t_elapsed += time.clock() - st
+        print '%s: %s' % (s, str(um.get_next(s)))
+
+    sim = lambda s: SHA().simMacro(s)
+    
+    # state
+    state_next = ila.choice('state_next', [state, ila.ite(cmddata == 1, m.const(1, 3), state)])
+    m.set_next('sha_state', state_next)
+
+    # xram
+    m.set_next('XRAM', xram)
+
+    # synthesize.
+    for s in ['sha_state', 'sha_rdaddr', 'sha_wraddr', 'sha_len']:
         st = time.clock()
         m.synthesize(s, sim)
-        t_elapsed = time.clock() - st
-        print 'time: %.2f' % (t_elapsed)
+        t_elapsed += time.clock() - st
 
         ast = m.get_next(s)
+        print '%s: %s' % (s, str(ast))
         m.exportOne(ast, 'asts/%s_%s' % (s, 'en' if enable_ps else 'dis'))
 
-    m.generateSim('tmp/shasim.hpp')
+    # connect to the uinst
+    m.connect_microabstraction('sha_state', um)
+    m.connect_microabstraction('XRAM', um)
+
+    print 'sha_state: %s' % str(m.get_next('sha_state'))
+    print 'XRAM: %s' % str(m.get_next('XRAM'))
+
+    print 'time: %.2f' % (t_elapsed)
 
 if __name__ == '__main__':
     ila.setloglevel(1, "")
     parser = argparse.ArgumentParser()
     parser.add_argument("--en", type=int, default=1, 
                         help="enable parameterized synthesis.")
-    parser.add_argument("state", type=str, nargs='+',
-                        help="the state to synthesize.")
+    #parser.add_argument("state", type=str, nargs='+',
+    #                    help="the state to synthesize.")
     args = parser.parse_args()
-    createSHAILA(args.state, args.en)
+    createSHAILA(args.en)
+
