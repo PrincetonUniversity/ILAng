a85e94bee6279d6aa83a2ea6455ca5979ade2103
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2016-05-17 18:56:01 -0400

    modified run scripts.

--- models/AES/rtlsim/run.sh
+++ models/AES/rtlsim/run.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-python synAES.py --en 0 ALL >& aes-dis.log
-python synAES.py --en 1 ALL >& aes-en.log
 python synAESuinst.py --en 1 >& aes-uinst-en.log
 python synAESuinst.py --en 0 >& aes-uinst-dis.log
