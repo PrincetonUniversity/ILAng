296dbbdb74169a2c3c156229041319959c7e0156
Bo-Yuan-Huang <byhuang1992@gmail.com>
2017-03-02 16:15:10 -0500

    Integrate ILA and HLS into one top module

--- apps/acc_spec/gaussian_hls/eq_verilog/eq_top.v
+++ apps/acc_spec/gaussian_hls/eq_verilog/eq_top.v
@@ -9,6 +9,17 @@ module eq_top (
     arg_0_TREADY,
     arg_1_TDATA,
     arg_1_TVALID,
+    //
+    hls_s_axi_config_AWVALID,
+    hls_s_axi_config_AWADDR,
+    hls_s_axi_config_WVALID,
+    hls_s_axi_config_WDATA,
+    hls_s_axi_config_WSTRB,
+    hls_s_axi_config_ARVALID,
+    hls_s_axi_config_ARADDR,
+    hls_s_axi_config_RREADY,
+    hls_s_axi_config_BREADY,
+    hls_arg_1_TLAST,
     // common output states
     ila_arg_0_TDATA,
     ila_arg_0_TVALID,
@@ -17,6 +28,14 @@ module eq_top (
     hls_arg_0_TVALID,
     hls_arg_1_TREADY
 );
+
+parameter    ap_const_int64_8 = 8;
+parameter    C_S_AXI_CONFIG_DATA_WIDTH = 32;
+parameter    C_S_AXI_CONFIG_ADDR_WIDTH = 5;
+parameter    C_S_AXI_DATA_WIDTH = 32;
+parameter    C_S_AXI_ADDR_WIDTH = 32;
+parameter    C_S_AXI_CONFIG_WSTRB_WIDTH = (C_S_AXI_CONFIG_DATA_WIDTH / ap_const_int64_8);
+parameter    C_S_AXI_WSTRB_WIDTH = (C_S_AXI_DATA_WIDTH / ap_const_int64_8);
 
 input           clk;
 input           rst_init;
@@ -31,6 +50,17 @@ output          ila_arg_1_TREADY;
 output  [7:0]   hls_arg_0_TDATA;
 output          hls_arg_0_TVALID;
 output          hls_arg_1_TREADY;
+
+input   hls_s_axi_config_AWVALID;
+input  [C_S_AXI_CONFIG_ADDR_WIDTH - 1 : 0] hls_s_axi_config_AWADDR;
+input   hls_s_axi_config_WVALID;
+input  [C_S_AXI_CONFIG_DATA_WIDTH - 1 : 0] hls_s_axi_config_WDATA;
+input  [C_S_AXI_CONFIG_WSTRB_WIDTH - 1 : 0] hls_s_axi_config_WSTRB;
+input   hls_s_axi_config_ARVALID;
+input  [C_S_AXI_CONFIG_ADDR_WIDTH - 1 : 0] hls_s_axi_config_ARADDR;
+input   hls_s_axi_config_RREADY;
+input   hls_s_axi_config_BREADY;
+input  [0:0] hls_arg_1_TLAST;
 
 wire ila_clk = clk & ila_step;
 wire hls_clk = clk & hls_step;
@@ -56,7 +86,7 @@ wire    [9:0]   ila_y_idx;
 wire    [71:0]  ila_slice_buff;
 wire            ila_slice_full;
 
-gaussianRTL ila_target (
+gaussianRTL ila_target_U (
     .arg_0_TREADY (arg_0_TREADY),
     .arg_1_TDATA (arg_1_TDATA),
     .arg_1_TVALID (arg_1_TVALID),
@@ -83,7 +113,61 @@ gaussianRTL ila_target (
     .step (ila_step)
 );
 
+// hls
+wire hls_s_axi_config_AWVALID;
+wire hls_s_axi_config_AWREADY;
+wire [C_S_AXI_CONFIG_ADDR_WIDTH - 1 : 0] hls_s_axi_config_AWADDR;
+wire hls_s_axi_config_WVALID;
+wire hls_s_axi_config_WREADY;
+wire [C_S_AXI_CONFIG_DATA_WIDTH - 1 : 0] hls_s_axi_config_WDATA;
+wire [C_S_AXI_CONFIG_WSTRB_WIDTH - 1 : 0] hls_s_axi_config_WSTRB;
+wire hls_s_axi_config_ARVALID;
+wire hls_s_axi_config_ARREADY;
+wire [C_S_AXI_CONFIG_ADDR_WIDTH - 1 : 0] hls_s_axi_config_ARADDR;
+wire hls_s_axi_config_RVALID;
+wire hls_s_axi_config_RREADY;
+wire [C_S_AXI_CONFIG_DATA_WIDTH - 1 : 0] hls_s_axi_config_RDATA;
+wire [1:0] hls_s_axi_config_RRESP;
+wire hls_s_axi_config_BVALID;
+wire hls_s_axi_config_BREADY;
+wire [1:0] hls_s_axi_config_BRESP;
+wire hls_interrupt;
+wire [7:0] hls_arg_0_TDATA;
+wire [0:0] hls_arg_0_TLAST;
+wire [0:0] hls_arg_1_TLAST;
+wire hls_arg_1_TREADY;
+wire hls_arg_0_TVALID;
 
+hls_target hls_target_U (
+    .s_axi_config_AWVALID (hls_s_axi_config_AWVALID),
+    .s_axi_config_AWREADY (hls_s_axi_config_AWREADY),
+    .s_axi_config_AWADDR (hls_s_axi_config_AWADDR),
+    .s_axi_config_WVALID (hls_s_axi_config_WVALID),
+    .s_axi_config_WREADY (hls_s_axi_config_WREADY),
+    .s_axi_config_WDATA (hls_s_axi_config_WDATA),
+    .s_axi_config_WSTRB (hls_s_axi_config_WSTRB),
+    .s_axi_config_ARVALID (hls_s_axi_config_ARVALID),
+    .s_axi_config_ARREADY (hls_s_axi_config_ARREADY),
+    .s_axi_config_ARADDR (hls_s_axi_config_ARADDR),
+    .s_axi_config_RVALID (hls_s_axi_config_RVALID),
+    .s_axi_config_RREADY (hls_s_axi_config_RREADY),
+    .s_axi_config_RDATA (hls_s_axi_config_RDATA),
+    .s_axi_config_RRESP (hls_s_axi_config_RRESP),
+    .s_axi_config_BVALID (hls_s_axi_config_BVALID),
+    .s_axi_config_BREADY (hls_s_axi_config_BREADY),
+    .s_axi_config_BRESP (hls_s_axi_config_BRESP),
+    .ap_clk (hls_clk),
+    .ap_rst_n (~rst),
+    .interrupt (hls_interrupt),
+    .arg_0_TDATA (hls_arg_0_TDATA),
+    .arg_0_TLAST (hls_arg_0_TLAST),
+    .arg_1_TDATA (arg_1_TDATA),
+    .arg_1_TLAST (hls_arg_1_TLAST),
+    .arg_1_TVALID (arg_1_TVALID),
+    .arg_1_TREADY (hls_arg_1_TREADY),
+    .arg_0_TVALID (hls_arg_0_TVALID),
+    .arg_0_TREADY (arg_0_TREADY)
+);
 
 
 endmodule
