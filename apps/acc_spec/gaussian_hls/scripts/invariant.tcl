41613c0adfb6336bcb410954993e551a90f42b30
Bo-Yuan-Huang <byhuang1992@gmail.com>
2017-03-13 19:06:00 -0400

    Unclear Loop mechanism and need to modify abstraction decode functions.

--- apps/acc_spec/gaussian_hls/scripts/invariant.tcl
+++ apps/acc_spec/gaussian_hls/scripts/invariant.tcl
@@ -32,6 +32,18 @@ assume -name {inv - x/y/w index} -env \
     hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.hls_target_call_Loop_LB2D_shift_proc_U0.i_0_i_i_reg_152 <= 488 \
 )} -type {temporary} -update_db;
 
+# y idx smaller then 8 --> slice empty
+assume -name {inv - y/slice - ila} -env \
+{ ( \
+    (ila_U.LB2D_proc_y <= 7 |-> ila_U.slice_stream_empty == 1) \
+)} -type {temporary} -update_db;
+
+assume -name {inv - y/slice - hls} -env \
+{ ( \
+    (hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.hls_target_call_Loop_LB2D_buf_proc_U0.col_reg_349 <= 7 |-> \
+     hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.slice_stream_V_value_V_U.internal_empty_n == 0) \
+)} -type {temporary} -update_db;
+
 # micro-arch states
 # internal fsm state encoding
 assume -name {inv - cs/ns fsm} -env \
@@ -76,8 +88,28 @@ assume -name {inv - in stream pointer} -env \
      hls_U.hls_target_linebuffer_1_U0.in_stream_V_value_V_U.internal_empty_n == 1 & \
      hls_U.hls_target_linebuffer_1_U0.in_stream_V_value_V_U.mOutPtr == 0) | \
     (hls_U.hls_target_linebuffer_1_U0.in_stream_V_value_V_U.internal_full_n  == 0 & \
-     hls_U.hls_target_linebuffer_1_U0.in_stream_V_value_V_U.internal_empty_n == 0 & \
+     hls_U.hls_target_linebuffer_1_U0.in_stream_V_value_V_U.internal_empty_n == 1 & \
      hls_U.hls_target_linebuffer_1_U0.in_stream_V_value_V_U.mOutPtr == 1) \
+)} -type {temporary} -update_db;
+
+# iteration interal states 
+# TODO
+assume -name {inv - ppiten} -env \
+{ ( \
+    hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_Loop_1_proc_U0.ap_reg_ppiten_pp0_it0 == 1 & \
+    hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_Loop_1_proc_U0.ap_reg_ppiten_pp0_it1 == 1 \
+)} -type {temporary} -update_db;
+
+# axi config
+assume -name {inv - axi config} -env \
+{ ( \
+    hls_U.ap_start == 1 & \
+    hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_Loop_1_proc_U0.in_axi_stream_V_value_V_0_areset_d == 0 \
+)} -type {temporary} -update_db;
+
+assume -name {inv -axi config init} -env \
+{ counter == 0 |-> ( \
+    hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_Loop_1_proc_U0.ap_done_reg == 0 \
 )} -type {temporary} -update_db;
 
 # time-out counter
@@ -90,4 +122,23 @@ assume -name {inv - time out} -env \
     (hls_U.hls_target_Loop_1_proc_U0.indvar_flatten_reg_434 < 100) & \
     (hls_U.hls_target_Loop_1_proc_U0.indvar_flatten_next_fu_473_p2 < 100 ) \
 )} -type {temporary} -update_db;
+
+assume -name {tmp - block corner case} -env \
+{ counter == 0 |=> ( \
+    ila_U.LB2D_proc_x < 100 & \
+    ila_U.LB2D_proc_x > 8 & \
+    ila_U.LB2D_shift_x < 100 & \
+    ila_U.LB2D_shift_x > 8 & \
+    hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.hls_target_call_Loop_LB2D_buf_proc_U0.exitcond3_reg_702 == 0 & \
+    hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.hls_target_call_Loop_LB2D_buf_proc_U0.row_reg_327 >= 8 & \
+    hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.hls_target_call_Loop_LB2D_buf_proc_U0.row_reg_327 < 100 & \
+    hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.hls_target_call_Loop_LB2D_buf_proc_U0.row_1_reg_693 >= 8 & \
+    hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.hls_target_call_Loop_LB2D_buf_proc_U0.row_1_reg_693 < 100 & \
+    (hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.hls_target_call_Loop_LB2D_buf_proc_U0.icmp_reg_698 == \
+     hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.hls_target_call_Loop_LB2D_buf_proc_U0.icmp_fu_382_p2) & \
+    (hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.hls_target_call_Loop_LB2D_shift_proc_U0.icmp_reg_1260 == \
+     hls_U.hls_target_linebuffer_1_U0.hls_target_linebuffer_U0.hls_target_call_U0.hls_target_call_Loop_LB2D_shift_proc_U0.icmp_fu_197_p2) & \
+    hls_U.hls_target_Loop_1_proc_U0.p_p2_in_bounded_stencil_stream_V_value_V_read == 0 \
+)} -type {temporary} -update_db;
+
 
