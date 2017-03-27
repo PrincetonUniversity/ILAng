099058e10539095e5b58a5d71e3678731e72d3f2
Bo-Yuan-Huang <byhuang1992@gmail.com>
2017-03-27 16:55:52 -0400

    Fix the RAM access pattern from sequence

--- apps/acc_spec/gaussian_hls/ila/gb_u2.py
+++ apps/acc_spec/gaussian_hls/ila/gb_u2.py
@@ -150,8 +150,9 @@ def U2 (gb):
     def genSliceSeqs (start):
         assert (start <= gb.LB2D_proc_size)
         res = []
-        for i in xrange (0, gb.LB2D_proc_size):
-            res.append ((start + i + 7) % gb.LB2D_proc_size)
+        #for i in xrange (0, gb.LB2D_proc_size):
+        for i in xrange (gb.LB2D_proc_size-1, -1, -1):
+            res.append ((start + i) % gb.LB2D_proc_size)
         return res
 
     slice_seqs = []
