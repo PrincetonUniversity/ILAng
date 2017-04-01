0213ff92b3726a0eb6677dc4f756d0b88ab5fa34
Bo-Yuan-Huang <byhuang1992@gmail.com>
2017-04-01 00:36:37 -0400

    Setup proof environment for spec A, need to release constraints

--- apps/acc_spec/gaussian_hls/scripts_spec_a/exec_w.tcl
+++ apps/acc_spec/gaussian_hls/scripts_spec_a/exec_w.tcl
@@ -8,3 +8,59 @@ assume -name {exec - start model} -env \
     ila_complete == 0 \
 )} -type {temporary} -update_db;
 
+# stop
+assume -name {exec - ila continue} -env \
+{ (counter > 1) |-> ( \
+    (ila_arg_1_TREADY == 1 & \
+     ila_arg_0_TVALID == 0 & \
+     arg_1_TVALID == 1 & \
+     arg_0_TREADY == 0) | \
+    (ila_arg_1_TREADY == 0 & \
+     ila_arg_0_TVALID == 0 & \
+     arg_0_TREADY == 0) \
+    |=> \
+    (ila_complete == 0) \
+)} -type {temporary} -update_db;
+#
+assume -name {exec - ila break} -env \
+{ (counter > 1) |-> ( \
+   ~( \
+    (ila_arg_1_TREADY == 1 & \
+     ila_arg_0_TVALID == 0 & \
+     arg_1_TVALID == 1 & \
+     arg_0_TREADY == 0) | \
+    (ila_arg_1_TREADY == 0 & \
+     ila_arg_0_TVALID == 0 & \
+     arg_0_TREADY == 0) \
+    ) \
+    |=> \
+    (ila_complete == 1) \
+)} -type {temporary} -update_db;
+#
+assume -name {exec - hls continue} -env \
+{ (counter > 1) |-> ( \
+    (hls_arg_1_TREADY == 1 & \
+     hls_arg_0_TVALID == 0 & \
+     arg_1_TVALID == 1 & \
+     arg_0_TREADY == 0) | \
+    (hls_arg_1_TREADY == 0 & \
+     hls_arg_0_TVALID == 0 & \
+     arg_0_TREADY == 0) \
+    |=> \
+    (hls_complete == 0) \
+)} -type {temporary} -update_db;
+#
+assume -name {exec - hls break} -env \
+{ (counter > 1) |-> ( \
+   ~( \
+    (hls_arg_1_TREADY == 1 & \
+     hls_arg_0_TVALID == 0 & \
+     arg_1_TVALID == 1 & \
+     arg_0_TREADY == 0) | \
+    (hls_arg_1_TREADY == 0 & \
+     hls_arg_0_TVALID == 0 & \
+     arg_0_TREADY == 0) \
+    ) \
+    |=> \
+    (hls_complete == 1) \
+)} -type {temporary} -update_db;
