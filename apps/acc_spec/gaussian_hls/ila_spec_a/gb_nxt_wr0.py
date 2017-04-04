927506b0b2b21a67d5f7a0bcdec19370d9ddf8b2
Bo-Yuan-Huang <byhuang1992@gmail.com>
2017-04-04 15:12:06 -0400

    Forward RAM_x vs output valid condition

--- apps/acc_spec/gaussian_hls/ila_spec_a/gb_nxt_wr0.py
+++ apps/acc_spec/gaussian_hls/ila_spec_a/gb_nxt_wr0.py
@@ -47,8 +47,9 @@ def WRU1 (gb):
     arg_1_TREADY_nxt = READY_T
     gb.arg_1_TREADY_nxt = ila.ite (decode, arg_1_TREADY_nxt, gb.arg_1_TREADY_nxt)
 
-    arg_0_TVALID_nxt = ila.ite ((gb.RAM_x > gb.stencil_size - 1) & \
-                                (gb.RAM_y > gb.RAM_size - 1),
+    arg_0_TVALID_nxt = ila.ite (((gb.RAM_x > gb.stencil_size) | \
+                                 (gb.RAM_x == gb.RAM_x_1)) & \
+                                (gb.RAM_y >= gb.RAM_size),
                                 VALID_T, VALID_F)
     gb.arg_0_TVALID_nxt = ila.ite (decode, arg_0_TVALID_nxt, gb.arg_0_TVALID_nxt)
 
