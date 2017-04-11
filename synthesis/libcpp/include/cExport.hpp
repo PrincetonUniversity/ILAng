bcdcb97e83510905c9d8b0ba3ff31f929b3d91f3
Bo-Yuan-Huang <byhuang1992@gmail.com>
2017-04-11 15:06:02 -0400

    Start developing C export

---
+++ synthesis/libcpp/include/cExport.hpp
@@ -0,0 +1,16 @@
+#ifndef __CEXPORT_HPP_DEFINED
+#define __CEXPORT_HPP_DEFINED
+
+#include <ast.hpp>
+#include <util.hpp>
+#include <exception.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <fstream>
+
+namespace ila
+{
+    // TODO
+}
+
+#endif
---
+++ synthesis/libcpp/src/cExport.cpp
@@ -0,0 +1,8 @@
+#include <cExport.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string.hpp>
+
+namespace ila
+{
+    // TODO
+}
