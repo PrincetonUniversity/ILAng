65f00507ffff66b32d718bdc1cc522f97c1ab395
Bo-Yuan-Huang <byhuang1992@gmail.com>
2017-04-16 01:08:22 -0400

    Run 1 hour proof for spec a vs impl

---
+++ apps/acc_spec/gaussian_hls/scripts_impl_a/keep_w.tcl
@@ -0,0 +1,51 @@
+set ROOT [pwd]
+set SRC $ROOT/verilog
+set TCL $ROOT/scripts_impl_a
+set EXP $ROOT/exps_impl_a
+
+#restore -jdb $EXP/wri_A.jdb
+#set_max_trace_length 0; set_prove_per_property_max_time_limit 60m
+#prove -all
+#save -jdb $EXP/wri_A.jdb -capture_setup -capture_session_data
+
+#restore -jdb $EXP/wri_C3.jdb
+#set_max_trace_length 0; set_prove_per_property_max_time_limit 60m
+#prove -all
+#save -jdb $EXP/wri_C3.jdb -capture_setup -capture_session_data
+
+#restore -jdb $EXP/wri_C4.jdb
+#set_max_trace_length 0; set_prove_per_property_max_time_limit 60m
+#prove -all
+#save -jdb $EXP/wri_C4.jdb -capture_setup -capture_session_data
+
+#restore -jdb $EXP/wri_E.jdb
+#set_max_trace_length 0; set_prove_per_property_max_time_limit 60m
+#prove -all
+#save -jdb $EXP/wri_E.jdb -capture_setup -capture_session_data
+
+#
+
+#restore -jdb $EXP/wri_C1.jdb
+#set_max_trace_length 0; set_prove_per_property_max_time_limit 60m
+#prove -all
+#save -jdb $EXP/wri_C1.jdb -capture_setup -capture_session_data
+
+#restore -jdb $EXP/wri_C2.jdb
+#set_max_trace_length 0; set_prove_per_property_max_time_limit 60m
+#prove -all
+#save -jdb $EXP/wri_C2.jdb -capture_setup -capture_session_data
+
+#
+
+#restore -jdb $EXP/wri_B.jdb
+#set_max_trace_length 0; set_prove_per_property_max_time_limit 60m
+#prove -all
+#save -jdb $EXP/wri_B.jdb -capture_setup -capture_session_data
+
+#restore -jdb $EXP/wri_D.jdb
+#set_max_trace_length 0; set_prove_per_property_max_time_limit 60m
+#prove -all
+#save -jdb $EXP/wri_D.jdb -capture_setup -capture_session_data
+
+#exit
+
