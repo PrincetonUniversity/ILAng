228f8876089a2cc4a63559bf49036b6f95f5265a
Bo-Yuan-Huang <byhuang1992@gmail.com>
2017-04-19 01:44:42 -0400

    Scripts for proving spec a vs b after fixing proc_in corner case

--- apps/acc_spec/gaussian_hls/scripts_a_b/run_w.tcl
+++ apps/acc_spec/gaussian_hls/scripts_a_b/run_w.tcl
@@ -6,6 +6,7 @@ set EXP $ROOT/exps_a_b
 include $TCL/setup_w.tcl
 include $TCL/eqcheck_w.tcl
 
+set_engine_mode {Hp Ht B N}
 set_prove_time_limit 0s; set_prove_per_property_time_limit 0s
 set_max_trace_length 0; set_prove_per_property_max_time_limit 60m
 
@@ -63,4 +64,3 @@ assume -disable {::Subset - E}
 #assume -disable {::Subset - C2}
 
 #exit
-
