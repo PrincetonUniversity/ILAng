5fb105e9447574c94eccd228daf1b7eb7804415f
Bo-Yuan-Huang <byhuang1992@gmail.com>
2017-12-07 15:01:43 -0500

    Create initialization for top level ila infrastructure

---
+++ include/top.h
@@ -0,0 +1,35 @@
+/// \file
+/// The top level class for wrapping the ILA infrastructure.
+
+#ifndef __ILA_TOP_H__
+#define __ILA_TOP_H__
+
+#include "util/log.h"
+
+namespace ila {
+
+class IlaTop() {
+public:
+  /// Constructor for IlaTop.
+  IlaTop();
+  /// Destructor for IlaTop.
+  ~IlaTop();
+
+  /// \brief Initializating the ILA infrastructure.
+  /// Initialize the logging system (including glog and debug log).
+  void InitIlaInfra();
+
+  /// \brief Clear up the ILA infrastructure.
+  /// Clear up and reset the logging system (especially the debug log).
+  void CloseIlaInfra();
+
+private:
+  /// The (only) logging system.
+  Logger log_;
+
+}; // class IlaTop
+
+} // namespace ila
+
+#endif // __ILA_TOP_H__
+
--- src/top.cc
+++ src/top.cc
@@ -1,4 +1,23 @@
 /// \file
 /// The top level wrapper for the ILA infrastructure.
 
-// TODO: initialization, e.g. InitIlaLogging(), etc
+#include "top.h"
+
+namespace ila {
+
+IlaTop::IlaTop() { InitIlaInfra(); }
+
+IlaTop::~IlaTop() { CloseIlaInfra(); }
+
+void IlaTop::InitIlaInfra() {
+  log_.InitGlog();
+  log_.InitDlog();
+}
+
+void IlaTop::CloseIlaInfra() {
+  log_.CloseGlog();
+  log_.CloseDlog();
+}
+
+} // namespace ila
+
