a60841fff9874198f282597c7e0bf287b9529c6f
Bo-Yuan-Huang <byhuang1992@gmail.com>
2017-12-15 12:11:51 -0500

    Header for Sort class.

--- include/ila/ast/sort.h
+++ include/ila/ast/sort.h
@@ -5,6 +5,7 @@
 #define __SORT_H__
 
 #include "ila/ast.h"
+#include <vector>
 
 /// \namespace ila
 namespace ila {
@@ -16,32 +17,65 @@ typedef enum { SORT_BOOL, SORT_BV, SORT_ARRAY, SORT_APP } SortType;
 /// The class for sort (type for expr, and the range/domain of functions).
 class Sort : public Ast {
 public:
+  /// \def SortPtr
+  typedef std::shared_ptr<Sort> SortPtr;
+  /// \def SortPtrVec
+  typedef std::vector<SortPtr> SortPtrVec;
+
   // ------------------------- CONSTRUCTOR/DESTRUCTOR ----------------------- //
-  /// Default constructor.
+  /// Constructor for Boolean type.
   Sort();
+  /// Constructor for Bitvector type.
+  Sort(const int& bit_width);
+  /// Constructor for Array (Memory) type.
+  Sort(const int& addr_width, const int& data_width);
+  /// Constructor for Application type.
+  Sort(const SortPtr& out_sort, const SortPtrVec& args_sort);
   /// Default destructor.
   ~Sort();
 
   // ------------------------- ACCESSORS/MUTATORS --------------------------- //
 
   // ------------------------- METHODS -------------------------------------- //
-  /// Is Bool sort.
+  /// Return true if this is a Boolean expression.
   bool IsBool() const;
-  /// Is Bitvector sort.
+  /// Return true if this is a Bitvector expression.
   bool IsBv() const;
-  /// Is Array sort.
+  /// Return true if this is an Array expression.
   bool IsArray() const;
-  /// Is Application sort.
+  /// Return true if this is an Application expression.
   bool IsApp() const;
-  /// Is well sorted.
-  bool IsWellSorted(const Sort& srt) const;
+
+  /// Output to stream.
+  std::ostream& Print(std::ostream& out) const;
+
+  /// Overload output stream operator.
+  friend std::ostream& operator<<(std::ostream& out);
 
 private:
   // ------------------------- MEMBERS -------------------------------------- //
   /// The type for the sort.
   SortType type_;
+  /// Bit width of bitvector.
+  int bit_width_;
+  /// Address width of array.
+  int addr_width_;
+  /// Data width of array.
+  int data_width_;
+  /// Sort of the output data of application.
+  SortPtr out_sort_;
+  /// Sorts of the application arguments.
+  SortPtrVec args_sort_;
 
   // ------------------------- HELPERS -------------------------------------- //
+  /// Print Boolean type sort.
+  std::ostream& PrintBool(std::ostream& out) const;
+  /// Print Bitvector type sort.
+  std::ostream& PrintBv(std::ostream& out) const;
+  /// Print Array type sort.
+  std::ostream& PrintArray(std::ostream& out) const;
+  /// Print Application type sort.
+  std::ostream& PrintApp(std::ostream& out) const;
 
 }; // class Sort
 
---
+++ src/ila/ast/sort.cc
@@ -0,0 +1,13 @@
+/// \file
+/// Source for the class Sort
+
+#include "ila/ast/sort.h"
+
+namespace ila {
+
+Sort::Sort() {}
+
+Sort::~Sort() {}
+
+} // namespace ila
+
