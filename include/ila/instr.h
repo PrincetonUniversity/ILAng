b7577a4b290afe361cbd2ca6d80b66ac4596adf1
Bo-Yuan-Huang <byhuang1992@gmail.com>
2017-12-21 22:03:51 -0500

    Define the interface for instruction

--- include/ila/instr.h
+++ include/ila/instr.h
@@ -4,53 +4,83 @@
 #ifndef __INSTR_H__
 #define __INSTR_H__
 
-#include "ila/node.h"
+#include "ila/expr_fuse.h"
 #include "ila/object.h"
+#include "ila/simplify.h"
 #include <memory>
-#include <set>
 #include <string>
 
 /// \namespace ila
 namespace ila {
 
 /// \class Instr
-/// The class for the Instruction. An Instr object should contain the decode
-/// function and the state update functions for all the state variables. It can
-/// optionally have some extra attributes, e.g. sub/micro, view, etc.
+/// The class for the Instruction. An Instr object contains:
+/// - the decode function
+/// - a set of undate functions for the state variables
+/// - several attributes, e.g. view
 class Instr : public Object {
 public:
   // ------------------------- CONSTRUCTOR/DESTRUCTOR ----------------------- //
-  /// Default constructor. DO NOT USE.
-  Instr();
   /// Constructor with name.
-  Instr(const std::string& name);
+  Instr(const std::string& name, bool simplify, ExprSimplifier& expr_mngr);
   /// Default destructor.
   ~Instr();
 
   /// \def InstrPtr
   typedef std::shared_ptr<Instr> InstrPtr;
-  /// \def Instr specific type for the set of node pointers.
-  typedef std::set<NodePtr> NodePtrSet;
+  /// \def ExprPtrMap
+  typedef std::map<std::string, ExprPtr> ExprPtrMap;
 
   // ------------------------- ACCESSORS/MUTATORS --------------------------- //
-  /// Is type Instr.
+  /// Return true if Is type Instr.
   bool is_instr() const { return true; }
+  /// Return true if has view.
+  bool has_view() const;
 
   // ------------------------- METHODS -------------------------------------- //
+  /// Set the decode function.
+  /// \param[in] decode_expr is the pointer to the decode function (bool).
+  void SetDecode(const ExprPtr decode_expr);
+
+  /// Add one update function for the state variable specified by name.
+  /// \param[in] name the name of the state variable.
+  /// \param[in] update the update function expression (same type as state).
+  void AddUpdate(const std::string& name, const ExprPtr update);
+
+  /// Add one update function for the state variable specified by var pointer.
+  /// \param[in] state the pointer to the state variable.
+  /// \param[in] update the update function expression (same type as state).
+  void AddUpdate(const ExprPtr state, const ExprPtr update);
+
+  /// Return the decode function.
+  ExprPtr GetDecode() const;
+
+  /// Return the update function for the state specified by name.
+  /// \param[in] name the name of the state variable.
+  /// \return the state update function.
+  ExprPtr GetUpdate(const std::string& name) const;
+
+  /// Return the update function for the state specified by var pointer.
+  /// \param[in] state the pointer to the state variable.
+  ExprPtr GetUpdate(const ExprPtr state) const;
 
   /// Output function.
   virtual std::ostream& Print(std::ostream& out) const;
 
-  // Instr definition.
-
-  /// Set up decode function.
-  /// \param[in] decode_func pointer to the decode function.
-  bool SetDecode(NodePtr decode_func);
-
 private:
   // ------------------------- MEMBERS -------------------------------------- //
   /// Has view.
-  bool is_view_;
+  bool has_view_;
+  /// To simplify expr nodes.
+  bool to_simplify_;
+
+  /// The decode function.
+  ExprPtr decode_;
+  /// The set of update functions, mapped by name.
+  ExprPtrMap updates_;
+
+  /// The simplifier for expr nodes.
+  ExprSimplifier& expr_mngr_;
 
   // ------------------------- HELPERS -------------------------------------- //
 
--- src/ila/instr.cc
+++ src/ila/instr.cc
@@ -5,7 +5,12 @@
 
 namespace ila {
 
-Instr::Instr() {}
+Instr::Instr(const std::string& name, bool simplify, ExprSimplifier& expr_mngr)
+    : Object(name), to_simplify_(simplify), expr_mngr_(expr_mngr) {
+  decode_ = NULL;
+  has_view_ = false;
+  updates_.clear();
+}
 
 Instr::~Instr() {}
 
