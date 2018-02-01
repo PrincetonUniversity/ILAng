cf92c5ffb0d385875963ebd2c3915410f561fd77
byHuang <byhuang1992@gmail.com>
2018-02-02 00:27:46 +0800

    Model flush instruction

--- apps/spec-pipe/src/main.cc
+++ apps/spec-pipe/src/main.cc
@@ -39,11 +39,12 @@ auto ZERO = BvConst(0, REG_SIZE);
 auto TRUE = BoolConst(true);
 auto FALSE = BoolConst(false);
 
-enum OPCODE { NOP = 0, BR, LD, MV };
+enum OPCODE { NOP = 0, BR, LD, MV, FLUSH };
 auto OP_NOP = BvConst(OPCODE::NOP, OP_LENGTH);
 auto OP_BR = BvConst(OPCODE::BR, OP_LENGTH);
 auto OP_LD = BvConst(OPCODE::LD, OP_LENGTH);
 auto OP_MV = BvConst(OPCODE::MV, OP_LENGTH);
+auto OP_FLUSH = BvConst(OPCODE::FLUSH, OP_LENGTH);
 
 // ------------------------- Function Declaration --------------------------- //
 MemVal IrInitVal();
@@ -103,13 +104,14 @@ InstrLvlAbsPtr SpecExecIla() {
     auto reg_name = "reg" + std::to_string(i);
     regs.push_back(ila->NewBvState(reg_name, REG_SIZE));
   }
-  // micro-opcode (pipeline interface)
+  // execution opcode
   auto op_exec = ila->NewBvState("op_exec", OP_LENGTH);
-  auto op_comm = ila->NewBvState("op_comm", OP_LENGTH);
-  // micro-register-index
+  // execution register index
   auto idx_exec = ila->NewBvState("idx_exec", IDX_LENGTH);
-  // micro-immediate
+  // execution immediate
   auto imm_exec = ila->NewBvState("imm_exec", IMM_LENGTH);
+  // commit opcode
+  auto op_comm = ila->NewBvState("op_comm", OP_LENGTH);
 
   // ------------------------- Valid ---------------------------------------- //
   ila->SetValid(TRUE);
@@ -121,20 +123,19 @@ InstrLvlAbsPtr SpecExecIla() {
 
   // ------------------------- Inits ---------------------------------------- //
   { // pc, vpc, f_flush, op_exec, op_comm, idx_exec, imm_exec, regs, mem, cache
-    ila->AddInit(Eq(pc, ZERO));
-    ila->AddInit(Eq(vpc, ZERO));
-    ila->AddInit(Eq(f_flush, FALSE));
-    ila->AddInit(Eq(op_exec, OP_NOP));
-    ila->AddInit(Eq(op_comm, OP_NOP));
+    ila->AddInit(Eq(pc, ZERO));          // default value
+    ila->AddInit(Eq(vpc, ZERO));         // same as pc
+    ila->AddInit(Eq(f_flush, FALSE));    // no need to flush
+    ila->AddInit(Eq(op_exec, OP_FLUSH)); // no previous instr (not NOP)
+    ila->AddInit(Eq(op_comm, OP_FLUSH)); // no previous instr (not NOP)
 
-    for (auto i = 0; i != REG_NUM; i++) {
-      ila->AddInit(Eq(regs[i], ZERO));
-    }
+    for (auto i = 0; i != REG_NUM; i++)
+      ila->AddInit(Eq(regs[i], ZERO)); // initialize to be 0
 
     auto secret_addr = BvConst(SECRET_ADDR, REG_SIZE);
     auto secret_data = BvConst(SECRET_DATA, REG_SIZE);
-    ila->AddInit(Eq(secret_data, Load(mem, secret_addr)));
-    ila->AddInit(Ne(secret_data, Load(cache, secret_addr)));
+    ila->AddInit(Eq(secret_data, Load(mem, secret_addr)));   // contains secret
+    ila->AddInit(Ne(secret_data, Load(cache, secret_addr))); // no secret
   }
 
   // ------------------------- Instruction: Branch -------------------------- //
@@ -144,11 +145,11 @@ InstrLvlAbsPtr SpecExecIla() {
     instr->SetDecode(decode);
 
     // state updates
-    instr->AddUpdate(vpc, Add(vpc, ONE));
-    instr->AddUpdate(op_exec, OP_BR);
-    instr->AddUpdate(idx_exec, Extract(instr_w, IDX_HI, IDX_LO));
-    instr->AddUpdate(imm_exec, Extract(instr_w, IMM_HI, IMM_LO));
-  } // Branch
+    instr->AddUpdate(vpc, Add(vpc, ONE)); // speculatively not-taken
+    instr->AddUpdate(op_exec, OP_BR);     // pass down
+    instr->AddUpdate(idx_exec, Extract(instr_w, IDX_HI, IDX_LO)); // pass down
+    instr->AddUpdate(imm_exec, Extract(instr_w, IMM_HI, IMM_LO)); // pass down
+  }
 
   { // child-ILA for instruction Branch
     auto child = ila->NewChild("BranchChild");
@@ -174,11 +175,11 @@ InstrLvlAbsPtr SpecExecIla() {
         br_take_nxt =
             Ite(Eq(reg_idx_i, idx_exec), Eq(regs[i], ONE), br_take_nxt);
       }
-      instr->AddUpdate(br_take, br_take_nxt);
-      instr->AddUpdate(br_addr, imm_exec);
+      instr->AddUpdate(br_take, br_take_nxt); // compare with 1
+      instr->AddUpdate(br_addr, imm_exec);    // pass down
 
-      instr->AddUpdate(cache, cache);
-      instr->AddUpdate(op_comm, OP_BR);
+      instr->AddUpdate(cache, cache);   // unchanged
+      instr->AddUpdate(op_comm, OP_BR); // pass down
     }
 
     { // commit child-instruction
@@ -188,10 +189,10 @@ InstrLvlAbsPtr SpecExecIla() {
 
       // state updates
       auto pc_nxt = Ite(br_take, br_addr, Add(pc, ONE));
-      instr->AddUpdate(pc, pc_nxt);
-      instr->AddUpdate(mem, mem);
+      instr->AddUpdate(pc, pc_nxt); // update pc based on previous result
+      instr->AddUpdate(mem, mem);   // unchanged
       for (auto i = 0; i != REG_NUM; i++)
-        instr->AddUpdate(regs[i], regs[i]);
+        instr->AddUpdate(regs[i], regs[i]); // unchanged
       instr->AddUpdate(f_flush, br_take); // speculate not take (flush if take)
     }
   }
@@ -203,11 +204,11 @@ InstrLvlAbsPtr SpecExecIla() {
     instr->SetDecode(decode);
 
     // state updates
-    instr->AddUpdate(vpc, Add(vpc, ONE));
-    instr->AddUpdate(op_exec, OP_LD);
-    instr->AddUpdate(idx_exec, Extract(instr_w, IDX_HI, IDX_LO));
-    instr->AddUpdate(imm_exec, Extract(instr_w, IMM_HI, IMM_LO));
-  } // Load
+    instr->AddUpdate(vpc, Add(vpc, ONE)); // normal prediction
+    instr->AddUpdate(op_exec, OP_LD);     // pass down
+    instr->AddUpdate(idx_exec, Extract(instr_w, IDX_HI, IDX_LO)); // pass down
+    instr->AddUpdate(imm_exec, Extract(instr_w, IMM_HI, IMM_LO)); // pass down
+  }
 
   { // child-ILA for instruction Load
     auto child = ila->NewChild("LoadChild");
@@ -227,13 +228,13 @@ InstrLvlAbsPtr SpecExecIla() {
       instr->SetDecode(decode);
 
       // state updates
-      instr->AddUpdate(ld_idx, idx_exec);
+      instr->AddUpdate(ld_idx, idx_exec); // pass down
       auto addr = imm_exec;
       auto ld_data_nxt = Load(mem, addr);
-      instr->AddUpdate(ld_data, ld_data_nxt);
+      instr->AddUpdate(ld_data, ld_data_nxt); // pre-load data
 
-      instr->AddUpdate(cache, Store(cache, addr, ld_data_nxt));
-      instr->AddUpdate(op_comm, OP_LD);
+      instr->AddUpdate(cache, Store(cache, addr, ld_data_nxt)); // caching
+      instr->AddUpdate(op_comm, OP_LD);                         // pass down
     }
 
     { // comm child-instruction
@@ -242,13 +243,13 @@ InstrLvlAbsPtr SpecExecIla() {
       instr->SetDecode(decode);
 
       // state updates
-      instr->AddUpdate(pc, Add(pc, ONE));
-      instr->AddUpdate(mem, mem);
-      instr->AddUpdate(f_flush, FALSE);
+      instr->AddUpdate(pc, Add(pc, ONE)); // normal increment
+      instr->AddUpdate(mem, mem);         // unchanged
+      instr->AddUpdate(f_flush, FALSE);   // always success
       for (auto i = 0; i != REG_NUM; i++) {
         auto reg_i_nxt =
             Ite(Eq(ld_idx, BvConst(i, IDX_LENGTH)), ld_data, regs[i]);
-        instr->AddUpdate(regs[i], reg_i_nxt);
+        instr->AddUpdate(regs[i], reg_i_nxt); // only update specified register
       }
     }
   }
@@ -260,11 +261,11 @@ InstrLvlAbsPtr SpecExecIla() {
     instr->SetDecode(decode);
 
     // state updates
-    instr->AddUpdate(vpc, Add(vpc, ONE));
-    instr->AddUpdate(op_exec, OP_MV);
-    instr->AddUpdate(idx_exec, Extract(instr_w, IDX_HI, IDX_LO));
-    instr->AddUpdate(imm_exec, Extract(instr_w, IMM_HI, IMM_LO));
-  } // Move
+    instr->AddUpdate(vpc, Add(vpc, ONE)); // normal prediction
+    instr->AddUpdate(op_exec, OP_MV);     // pass down
+    instr->AddUpdate(idx_exec, Extract(instr_w, IDX_HI, IDX_LO)); // pass down
+    instr->AddUpdate(imm_exec, Extract(instr_w, IMM_HI, IMM_LO)); // pass down
+  }
 
   { // child-ILA for instruction Move
     auto child = ila->NewChild("MoveChild");
@@ -284,11 +285,11 @@ InstrLvlAbsPtr SpecExecIla() {
       instr->SetDecode(decode);
 
       // state updates
-      instr->AddUpdate(mv_idx, idx_exec);
-      instr->AddUpdate(mv_data, imm_exec);
+      instr->AddUpdate(mv_idx, idx_exec);  // pass down
+      instr->AddUpdate(mv_data, imm_exec); // pass down
 
-      instr->AddUpdate(cache, cache);
-      instr->AddUpdate(op_comm, OP_MV);
+      instr->AddUpdate(cache, cache);   // unchanged
+      instr->AddUpdate(op_comm, OP_MV); // pass down
     }
 
     { // comm child-instruction
@@ -297,14 +298,14 @@ InstrLvlAbsPtr SpecExecIla() {
       instr->SetDecode(decode);
 
       // state updates
-      instr->AddUpdate(pc, Add(pc, ONE));
-      instr->AddUpdate(mem, mem);
-      instr->AddUpdate(f_flush, FALSE);
+      instr->AddUpdate(pc, Add(pc, ONE)); // normal increment
+      instr->AddUpdate(mem, mem);         // unchanged
+      instr->AddUpdate(f_flush, FALSE);   // always success
 
       for (auto i = 0; i != REG_NUM; i++) {
         auto reg_i_nxt =
             Ite(Eq(mv_idx, BvConst(i, IDX_LENGTH)), mv_data, regs[i]);
-        instr->AddUpdate(regs[i], reg_i_nxt);
+        instr->AddUpdate(regs[i], reg_i_nxt); // only update the specified reg
       }
     }
   }
@@ -312,15 +313,16 @@ InstrLvlAbsPtr SpecExecIla() {
   // ------------------------- Instruction: NOP ----------------------------- //
   { // decode
     auto instr = ila->NewInstr("Nop");
+    // XXX try using negate form for totality
     auto decode = And(Eq(opcode, OP_NOP), Not(f_flush));
     instr->SetDecode(decode);
 
     // state updates
-    instr->AddUpdate(vpc, Add(vpc, ONE));
-    instr->AddUpdate(op_exec, OP_NOP);
-    instr->AddUpdate(idx_exec, idx_exec);
-    instr->AddUpdate(imm_exec, imm_exec);
-  } // Nop
+    instr->AddUpdate(vpc, Add(vpc, ONE)); // normal prediction
+    instr->AddUpdate(op_exec, OP_NOP);    // pass down
+    instr->AddUpdate(idx_exec, idx_exec); // don't care
+    instr->AddUpdate(imm_exec, imm_exec); // don't care
+  }
 
   { // child-ILA for instruction NOP
     auto child = ila->NewChild("NopChild");
@@ -331,29 +333,72 @@ InstrLvlAbsPtr SpecExecIla() {
 
     { // exec child-instruction
       auto instr = child->NewInstr("NopExec");
+      // XXX try using negate form for totality
       auto decode = And(Eq(op_exec, OP_NOP), Not(f_flush));
       instr->SetDecode(decode);
 
       // state updates
-      instr->AddUpdate(cache, cache);
-      instr->AddUpdate(op_comm, OP_NOP);
+      instr->AddUpdate(cache, cache);    // unchanged
+      instr->AddUpdate(op_comm, OP_NOP); // pass down
     }
 
     { // comm child-instruction
       auto instr = child->NewInstr("NopComm");
+      // XXX try using negate form for totality
       auto decode = And(Eq(op_comm, OP_NOP), Not(f_flush));
       instr->SetDecode(decode);
 
       // state updates
-      instr->AddUpdate(pc, Add(pc, ONE));
-      instr->AddUpdate(mem, mem);
-      instr->AddUpdate(f_flush, FALSE);
+      instr->AddUpdate(pc, Add(pc, ONE)); // normal increment
+      instr->AddUpdate(mem, mem);         // unchanged
+      instr->AddUpdate(f_flush, FALSE);   // always success
       for (auto i = 0; i != REG_NUM; i++)
-        instr->AddUpdate(regs[i], regs[i]);
+        instr->AddUpdate(regs[i], regs[i]); // unchanged
     }
   }
 
   // ------------------------- Instruction: FLUSH --------------------------- //
+  { // decode
+    auto instr = ila->NewInstr("Flush");
+    instr->SetDecode(f_flush);
+
+    // state updates
+    instr->AddUpdate(vpc, pc);            // reset pc
+    instr->AddUpdate(op_exec, OP_FLUSH);  // insert bubble
+    instr->AddUpdate(idx_exec, idx_exec); // don't care
+    instr->AddUpdate(imm_exec, imm_exec); // don't care
+  }
+
+  { // child-ILA for instruction FLUSH
+    auto child = ila->NewChild("FlushChild");
+    // valid
+    child->SetValid(TRUE);
+    // fetch
+    child->SetFetch(Concat(op_exec, op_comm));
+
+    { // exec child-instruction
+      auto instr = child->NewInstr("FlushExec");
+      auto decode = Or(f_flush, Eq(op_exec, OP_FLUSH));
+      instr->SetDecode(decode);
+
+      // state updates
+      instr->AddUpdate(cache, cache);      // unchanged
+      instr->AddUpdate(op_comm, OP_FLUSH); // pass down
+    }
+
+    { // comm child-instruction
+      auto instr = child->NewInstr("FlushComm");
+      auto decode = Or(f_flush, Eq(op_comm, OP_FLUSH));
+      instr->SetDecode(decode);
+
+      // state updates
+      instr->AddUpdate(pc, pc);         // unchanged!!
+      instr->AddUpdate(mem, mem);       // unchanged
+      instr->AddUpdate(f_flush, FALSE); // reset flushing flag
+      for (auto i = 0; i != REG_NUM; i++)
+        instr->AddUpdate(regs[i], regs[i]); // unchanged
+    }
+  }
 
   return ila;
 }
