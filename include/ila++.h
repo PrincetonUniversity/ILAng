61ac1626710c836340e55561b6d84df155270e30
Bo-Yuan-Huang <byhuang1992@gmail.com>
2018-02-13 10:33:46 -0500

    Cpp API for Instr

--- include/ila++.h
+++ include/ila++.h
@@ -4,18 +4,21 @@
 #ifndef __ILAPP_H__
 #define __ILAPP_H__
 
+#include <map>
 #include <memory>
 
 /// \namespace ila
 namespace ila {
 
 class Expr;
+class Instr;
+class InstrLvlAbs;
 
 /// \brief The reference of Expr (e.g. state var, var relation, constant, etc).
 class ExprRef {
 private:
   // ------------------------- MEMBERS -------------------------------------- //
-  /// Pointer to the actual data.
+  /// Wrapped Expr pointer.
   std::shared_ptr<Expr> ptr_;
 
 public:
@@ -25,18 +28,59 @@ public:
   /// Default destructor
   ~ExprRef();
 
+  // ------------------------- ACCESSORS/MUTATORS --------------------------- //
+  /// Return the wrapped Expr pointer.
+  inline std::shared_ptr<Expr> get() const { return ptr_; }
+
   // ------------------------- METHODS -------------------------------------- //
-  /// Overload unsigned add.
+  /// Arithmetic negate for bit-vector.
+  ExprRef operator-() const;
+  /// Unsigned add for bit-vector.
   ExprRef operator+(const ExprRef& rhs) const;
 
 }; // class NodeRef
 
-static ExprRef BoolConst(bool bool_val);
-static ExprRef BvConst(const int& bv_val, const int& bit_width);
+/// \brief Return a Boolean constant.
+/// \param[in] bool_val value of the Boolean constant.
+ExprRef BoolConst(bool bool_val);
+
+/// \brief Return a bit-vector constant.
+/// \param[in] bv_val value of the bit-vector constant.
+/// \param[in] bit_width data bit-width.
+ExprRef BvConst(const int& bv_val, const int& bit_width);
+
+/// \brief Return a memory constant.
+/// \param[in] def_val default value.
+/// \param[in] vals non-default address-data mapping.
+/// \param[in] addr_width address bit-width.
+/// \param[in] data_width data bit-width.
+ExprRef MemConst(const int& def_val, const std::map<int, int>& vals,
+                 const int& addr_width, const int& data_width);
 
 class InstrRef {
-  // TODO
-}; // class
+private:
+  // ------------------------- MEMBERS -------------------------------------- //
+  /// Wrapped Instr pointer.
+  std::shared_ptr<Instr> ptr_;
+
+public:
+  // ------------------------- CONSTRUCTOR/DESTRUCTOR ----------------------- //
+  /// Constructor with the pointer of the actual data.
+  InstrRef(std::shared_ptr<Instr> ptr);
+  /// Default destructor
+  ~InstrRef();
+
+  // ------------------------- METHODS -------------------------------------- //
+  /// \brief Set the decode function of the instruction.
+  /// \param[in] decode the Boolean typed decode function.
+  void SetDecode(const ExprRef& decode);
+
+  /// \brief Set the update function of the given state variable.
+  /// \param[in] state the state variable to set.
+  /// \param[in] update the update function (should be the same type as state).
+  void SetUpdate(const ExprRef& state, const ExprRef& update);
+
+}; // class InstrRef
 
 } // namespace ila
 
--- src/ila++.cc
+++ src/ila++.cc
@@ -6,13 +6,21 @@
 
 namespace ila {
 
+/******************************************************************************/
+// ExprRef
+/******************************************************************************/
 ExprRef::ExprRef(std::shared_ptr<Expr> ptr) : ptr_(ptr) {}
 
 ExprRef::~ExprRef() {}
 
+ExprRef ExprRef::operator-() const {
+  auto v = ExprFuse::Negate(get());
+  return ExprRef(v);
+}
+
 ExprRef ExprRef::operator+(const ExprRef& rhs) const {
-  auto s = ExprFuse::Add(ptr_, rhs.ptr_);
-  return ExprRef(s);
+  auto v = ExprFuse::Add(get(), rhs.get());
+  return ExprRef(v);
 }
 
 ExprRef BoolConst(bool val) {
@@ -23,6 +31,27 @@ ExprRef BoolConst(bool val) {
 ExprRef BvConst(const int& bv_val, const int& bit_width) {
   auto v = ExprFuse::BvConst(bv_val, bit_width);
   return ExprRef(v);
+}
+
+ExprRef MemConst(const int& def_val, const std::map<int, int>& vals,
+                 const int& addr_width, const int& data_width) {
+  auto v = ExprFuse::MemConst(MemVal(def_val, vals), addr_width, data_width);
+  return ExprRef(v);
+}
+
+/******************************************************************************/
+// InstrRef
+/******************************************************************************/
+InstrRef::InstrRef(std::shared_ptr<Instr> ptr) : ptr_(ptr) {}
+
+InstrRef::~InstrRef() {}
+
+void InstrRef::SetDecode(const ExprRef& decode) {
+  ptr_->SetDecode(decode.get());
+}
+
+void InstrRef::SetUpdate(const ExprRef& state, const ExprRef& update) {
+  ptr_->AddUpdate(state.get(), update.get());
 }
 
 } // namespace ila
