614796eedba8b4c2f14cb3dd3c8196d2766f72f1
Bo-Yuan-Huang <byhuang1992@gmail.com>
2018-02-15 15:37:26 -0500

    Update API manual page

--- docs/api-html/search/all_b.js
+++ docs/api-html/search/all_b.js
@@ -4,5 +4,5 @@ var searchData=
   ['setfetch',['SetFetch',['../classila_1_1_ila.html#adfbdbbd37df8a43109338ad81fe2de01',1,'ila::Ila']]],
   ['setupdate',['SetUpdate',['../classila_1_1_instr_ref.html#a3e7d1fa7ec35e49cfae9ffde207dcd20',1,'ila::InstrRef']]],
   ['setvalid',['SetValid',['../classila_1_1_ila.html#ab86bf4218b000460e005a13ce0a8a523',1,'ila::Ila']]],
-  ['store',['Store',['../classila_1_1_expr_ref.html#a6bacb4cb719da6513a9d45415db40a82',1,'ila::ExprRef']]]
+  ['store',['Store',['../classila_1_1_expr_ref.html#a6bacb4cb719da6513a9d45415db40a82',1,'ila::ExprRef::Store()'],['../namespaceila.html#ae170357be0b2d16f0ba5773b6aa209e7',1,'ila::Store()']]]
 ];
