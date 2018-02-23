d24e8de9a732acf682e64e565b4881a81d32d82d
Bo-Yuan-Huang <byhuang1992@gmail.com>
2018-02-22 23:33:52 -0500

    Clean up Sort files

--- include/ila/ast/sort.h
+++ include/ila/ast/sort.h
@@ -8,7 +8,6 @@
 #include "z3++.h"
 #include <memory>
 #include <ostream>
-#include <vector>
 
 /// \namespace ila
 namespace ila {
@@ -95,7 +94,6 @@ public:
   bool Equal(const SortPtr rhs) const;
   /// Print out to output stream.
   std::ostream& Print(std::ostream& out) const;
-
 }; // class SortBool
 
 /// \brief The class of bit-vector Sort.
@@ -110,6 +108,8 @@ public:
   // ------------------------- ACCESSORS/MUTATORS --------------------------- //
   /// Return true since it is bit-vector Sort.
   bool is_bv() const { return true; }
+
+  // ------------------------- METHODS -------------------------------------- //
   /// Return the bit-width.
   int bit_width() const { return bit_width_; }
   /// Return the z3::sort of bit-vector.
@@ -122,200 +122,39 @@ public:
 private:
   /// Bit-width of the bit-vector.
   int bit_width_;
-
 }; // class SortBv
 
+/// \brief The class of memory (array) Sort.
 class SortMem : public Sort {
 public:
+  // ------------------------- CONSTRUCTOR/DESTRUCTOR ----------------------- //
+  /// Default constructor.
   SortMem(const int& addr_w, const int& data_w);
+  /// Default destructor.
   ~SortMem();
 
+  // ------------------------- ACCESSORS/MUTATORS --------------------------- //
+  /// Return true since it is memory (array) Sort.
   bool is_mem() const { return true; }
 
+  // ------------------------- METHODS -------------------------------------- //
+  /// Return the bit-width of address (index).
   int addr_width() const { return addr_width_; }
-
+  /// Return the bit-width of data (value).
   int data_width() const { return data_width_; }
-
+  /// Return the z3::sort of array.
   z3::sort GetZ3Sort(z3::context& ctx) const;
-
+  /// Compare with another Sort.
   bool Equal(const SortPtr rhs) const;
-
+  /// Print out to output stream.
   std::ostream& Print(std::ostream& out) const;
 
 private:
+  /// Bit-width of the address.
   int addr_width_;
+  /// Bit-width of the data.
   int data_width_;
 }; // class SortMem
-
-#if 0
-/// \brief The class for sort (type for expr, and the range/domain of
-/// functions).
-class Sort {
-private:
-  SortBasePtr ptr_;
-
-public:
-  // ------------------------- CONSTRUCTOR/DESTRUCTOR ----------------------- //
-  /// Constructor for Boolean type.
-  Sort() { ptr_ = std::make_shared<SortBool>(); }
-  /// Constructor for Bitvector type.
-  Sort(const int& bit_width) { ptr_ = std::make_shared<SortBv>(bit_width); }
-  /// Constructor for Memory (Array) type.
-  Sort(const int& addr_width, const int& data_width) {
-    ptr_ = std::make_shared<SortMem>(addr_width, data_width);
-  }
-  /// Constructor for wrapping existed sort.
-  Sort(SortBasePtr s) : ptr_(s) {}
-  /// Default destructor.
-  ~Sort() {}
-
-  // ------------------------- ACCESSORS/MUTATORS --------------------------- //
-  /// Return the bit width (bitvector).
-  inline int bit_width() const { return ptr_->bit_width(); }
-  /// Return the address width (mem).
-  inline int addr_width() const { return ptr_->addr_width(); }
-  /// Return the data width (mem).
-  inline int data_width() const { return ptr_->data_width(); }
-
-  /// Is type sort (object).
-  inline bool is_sort() const { return ptr_->is_sort(); }
-
-  /// Return true if this is a Boolean expression.
-  inline bool is_bool() const { return ptr_->is_bool(); }
-  /// Return true if this is a Bitvector expression.
-  inline bool is_bv() const { return ptr_->is_bv(); }
-  /// Return true if this is an Memory expression.
-  inline bool is_mem() const { return ptr_->is_mem(); }
-
-  // ------------------------- METHODS -------------------------------------- //
-  /// Return z3 sort.
-  inline z3::sort GetZ3Sort(z3::context& ctx) const {
-    return ptr_->GetZ3Sort(ctx);
-  }
-
-  /// Output to stream.
-  inline std::ostream& Print(std::ostream& out) const {
-    return ptr_->Print(out);
-  }
-
-/// Compare two Sorts.
-#if 0
-  static bool Equal(const Sort& lhs, const Sort& rhs) {
-    return SortBase::Equal(lhs.ptr_, rhs.ptr_);
-  }
-#endif
-
-  /// Overload output stream operator.
-  friend std::ostream& operator<<(std::ostream& out, const Sort& s) {
-    return out << s.ptr_;
-  }
-  /// Overlaod comparison.
-  friend bool operator==(const Sort& lhs, const Sort& rhs) {
-    return lhs.ptr_ == rhs.ptr_;
-  }
-
-  /// Create Bool sort
-  static SortPtr MakeBoolSort() {
-    SortBasePtr s = std::make_shared<SortBool>();
-    return s;
-    // return Sort(s);
-  }
-  /// Create Bitvector sort.
-  static SortPtr MakeBvSort(const int& bit_width) {
-    SortBasePtr s = std::make_shared<SortBv>(bit_width);
-    return s;
-    // return Sort(s);
-  }
-  /// Create Memory sort.
-  static SortPtr MakeMemSort(const int& addr_width, const int& data_width) {
-    SortBasePtr s = std::make_shared<SortMem>(addr_width, data_width);
-    return s;
-    // return Sort(s);
-  }
-
-}; // class Sort
-
-#endif
-
-#if 0
-/// SortType
-typedef enum { SORT_BOOL, SORT_BV, SORT_MEM } SortType;
-
-/// \brief The class for sort (type for expr, and the range/domain of
-/// functions).
-class Sort : public Ast {
-public:
-  // ------------------------- CONSTRUCTOR/DESTRUCTOR ----------------------- //
-  /// Constructor for Boolean type.
-  Sort();
-  /// Constructor for Bitvector type.
-  Sort(const int& bit_width);
-  /// Constructor for Memory (Array) type.
-  Sort(const int& addr_width, const int& data_width);
-  /// Default destructor.
-  ~Sort();
-
-  // ------------------------- ACCESSORS/MUTATORS --------------------------- //
-  /// Return the bit width (bitvector).
-  inline const int& bit_width() const { return bit_width_; }
-  /// Return the address width (mem).
-  inline const int& addr_width() const { return addr_width_; }
-  /// Return the data width (mem).
-  inline const int& data_width() const { return data_width_; }
-
-  /// Is type sort (object).
-  bool is_sort() const { return true; }
-
-  /// Return true if this is a Boolean expression.
-  inline bool is_bool() const { return (type_ == SortType::SORT_BOOL); }
-  /// Return true if this is a Bitvector expression.
-  inline bool is_bv() const { return (type_ == SortType::SORT_BV); }
-  /// Return true if this is an Memory expression.
-  inline bool is_mem() const { return (type_ == SortType::SORT_MEM); }
-
-  // ------------------------- METHODS -------------------------------------- //
-  /// Return z3 sort.
-  z3::sort GetZ3Sort(z3::context& ctx) const;
-
-  /// Output to stream.
-  std::ostream& Print(std::ostream& out) const;
-  /// Compare two Sorts.
-  static bool Equal(const Sort& lhs, const Sort& rhs);
-
-  /// Overload output stream operator.
-  friend std::ostream& operator<<(std::ostream& out, const Sort& s);
-  /// Overlaod comparison.
-  friend bool operator==(const Sort& lhs, const Sort& rhs);
-
-  /// Create Bool sort
-  static Sort MakeBoolSort();
-  /// Create Bitvector sort.
-  static Sort MakeBvSort(const int& bit_width);
-  /// Create Memory sort.
-  static Sort MakeMemSort(const int& addr_width, const int& data_width);
-
-private:
-  // ------------------------- MEMBERS -------------------------------------- //
-  /// The type for the sort.
-  SortType type_;
-  /// Bit width of bitvector.
-  int bit_width_;
-  /// Address width of mem.
-  int addr_width_;
-  /// Data width of mem.
-  int data_width_;
-
-  // ------------------------- HELPERS -------------------------------------- //
-
-  /// Print Boolean type sort.
-  std::ostream& PrintBool(std::ostream& out) const;
-  /// Print Bitvector type sort.
-  std::ostream& PrintBv(std::ostream& out) const;
-  /// Print Array type sort.
-  std::ostream& PrintMem(std::ostream& out) const;
-
-}; // class Sort
-#endif
 
 } // namespace ila
 
--- src/ila/ast/sort.cc
+++ src/ila/ast/sort.cc
@@ -25,19 +25,14 @@ int Sort::data_width() const {
   return 0;
 }
 
-SortPtr Sort::MakeBoolSort() {
-  SortPtr s = std::make_shared<SortBool>();
-  return s;
-}
+SortPtr Sort::MakeBoolSort() { return std::make_shared<SortBool>(); }
 
 SortPtr Sort::MakeBvSort(const int& bit_width) {
-  SortPtr s = std::make_shared<SortBv>(bit_width);
-  return s;
+  return std::make_shared<SortBv>(bit_width);
 }
 
 SortPtr Sort::MakeMemSort(const int& addr_width, const int& data_width) {
-  SortPtr s = std::make_shared<SortMem>(addr_width, data_width);
-  return s;
+  return std::make_shared<SortMem>(addr_width, data_width);
 }
 
 SortBool::SortBool() {}
@@ -85,85 +80,6 @@ bool SortMem::Equal(const SortPtr rhs) const {
 std::ostream& SortMem::Print(std::ostream& out) const {
   return out << "Mem(" << addr_width_ << ", " << data_width_ << ")";
 }
-
-#if 0
-Sort::Sort()
-    : type_(SortType::SORT_BOOL), bit_width_(0), addr_width_(0),
-      data_width_(0) {}
-
-Sort::Sort(const int& bit_width)
-    : type_(SortType::SORT_BV), bit_width_(bit_width), addr_width_(0),
-      data_width_(0) {}
-
-Sort::Sort(const int& addr_width, const int& data_width)
-    : type_(SortType::SORT_MEM), bit_width_(0), addr_width_(addr_width),
-      data_width_(data_width) {}
-
-Sort::~Sort() {}
-
-z3::sort Sort::GetZ3Sort(z3::context& ctx) const {
-  if (is_bool())
-    return ctx.bool_sort();
-  else if (is_bv())
-    return ctx.bv_sort(bit_width_);
-  else {
-    ILA_ASSERT(is_mem()) << "Unknown sort.";
-    return ctx.array_sort(ctx.bv_sort(addr_width_), ctx.bv_sort(data_width_));
-  }
-}
-
-std::ostream& Sort::Print(std::ostream& out) const {
-  if (is_bool())
-    return PrintBool(out);
-  else if (is_bv())
-    return PrintBv(out);
-  else {
-    ILA_ASSERT(is_mem()) << "Unknown type.";
-    return PrintMem(out);
-  }
-}
-
-bool Sort::Equal(const Sort& lhs, const Sort& rhs) {
-  if (lhs.is_bool() && rhs.is_bool()) {
-    return true;
-  } else if (lhs.is_bv() && rhs.is_bv()) {
-    return (lhs.bit_width_ == rhs.bit_width_);
-  } else if (lhs.is_mem() && rhs.is_mem()) {
-    return ((lhs.addr_width_ == rhs.addr_width_) &&
-            (lhs.data_width_ == rhs.data_width_));
-  } else {
-    return false;
-  }
-}
-
-std::ostream& operator<<(std::ostream& out, const Sort& s) {
-  return s.Print(out);
-}
-
-bool operator==(const Sort& lhs, const Sort& rhs) {
-  return Sort::Equal(lhs, rhs);
-}
-
-Sort Sort::MakeBoolSort() { return Sort(); }
-
-Sort Sort::MakeBvSort(const int& bit_width) { return Sort(bit_width); }
-
-Sort Sort::MakeMemSort(const int& addr_width, const int& data_width) {
-  return Sort(addr_width, data_width);
-}
-
-std::ostream& Sort::PrintBool(std::ostream& out) const {
-  return out << "Boolean";
-}
-
-std::ostream& Sort::PrintBv(std::ostream& out) const {
-  return out << "Bv(" << bit_width_ << ")";
-}
-
-std::ostream& Sort::PrintMem(std::ostream& out) const {
-  return out << "Mem(" << addr_width_ << ", " << data_width_ << ")";
-}
-#endif
 
 } // namespace ila
 
