aad1f1ebb6910882db27105790175ced8c002b6e
Bo-Yuan-Huang <byhuang1992@gmail.com>
2018-02-22 23:55:51 -0500

    Unit test for mem Sort

--- test/t_sort.cc
+++ test/t_sort.cc
@@ -49,7 +49,7 @@ TEST(TestSort, Bitvector) {
   EXPECT_FALSE(s->is_func());
   EXPECT_FALSE(s->is_bool());
   EXPECT_TRUE(s->is_bv());
-  EXPECT_FALSE(s->is_bool());
+  EXPECT_FALSE(s->is_mem());
 
   EXPECT_EQ(8, s->bit_width());
 #ifndef NDEBUG
@@ -72,37 +72,34 @@ TEST(TestSort, Bitvector) {
   EXPECT_TRUE(z.is_bv());
 }
 
-#if 0
 TEST(TestSort, Memory) {
-  Sort mem_sort(2, 32);
-  EXPECT_TRUE(mem_sort.is_ast());
-  EXPECT_TRUE(mem_sort.is_mem());
-
-  EXPECT_EQ(0, mem_sort.bit_width());
-  EXPECT_EQ(2, mem_sort.addr_width());
-  EXPECT_EQ(32, mem_sort.data_width());
-
-  std::string msg;
-  GET_STDOUT_MSG(std::cout << mem_sort, msg);
-  EXPECT_EQ("Mem(2, 32)", msg);
-
-  Sort wrap = Sort::MakeMemSort(2, 32);
-  EXPECT_EQ(wrap, mem_sort);
-}
-#endif
-
-TEST(TestSort, Hierarchy) {
-  auto s = Sort::MakeBoolSort();
-  EXPECT_TRUE(s->is_bool());
+  auto s = Sort::MakeMemSort(2, 32);
+  EXPECT_TRUE(s->is_ast());
+  EXPECT_TRUE(s->is_sort());
+  EXPECT_FALSE(s->is_expr());
+  EXPECT_FALSE(s->is_func());
+  EXPECT_FALSE(s->is_bool());
   EXPECT_FALSE(s->is_bv());
-  EXPECT_FALSE(s->is_mem());
+  EXPECT_TRUE(s->is_mem());
+
 #ifndef NDEBUG
   EXPECT_DEATH(s->bit_width(), ".*");
 #else
-  auto w = s->bit_width();
+  EXPECT_EQ(0, s->bit_width());
 #endif
+  EXPECT_EQ(2, s->addr_width());
+  EXPECT_EQ(32, s->data_width());
 
-  auto m = Sort::MakeMemSort(8, 8);
+  std::string msg;
+  GET_STDOUT_MSG(std::cout << s, msg);
+  EXPECT_EQ("Mem(2, 32)", msg);
+
+  auto s2 = Sort::MakeMemSort(2, 32);
+  EXPECT_EQ(s, s2);
+
+  z3::context c;
+  auto z = s->GetZ3Sort(c);
+  EXPECT_TRUE(z.is_array());
 }
 
 } // namespace ila
