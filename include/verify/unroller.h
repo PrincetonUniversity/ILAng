5ee7fb61ca8b3edd50db12c31fe956b564a3dee1
Bo-Yuan-Huang <byhuang1992@gmail.com>
2018-02-23 16:05:23 -0500

    Generate z3 expr for update of one instr (complete)

--- include/verify/unroller.h
+++ include/verify/unroller.h
@@ -4,11 +4,37 @@
 #ifndef UNROLLER_H__
 #define UNROLLER_H__
 
+#include "ila/instr_lvl_abs.h"
+#include "ila/z3_expr_adapter.h"
+#include "z3++.h"
+
 namespace ila {
 
 /// \brief Class for unrolling ILA execution in different settings.
 class Unroller {
 public:
+  Unroller(z3::context& ctx);
+  virtual ~Unroller();
+
+#if 0
+  virtual z3::expr OneStepInstrNoDecode(const InstrPtr instr) const = 0;
+  virtual z3::expr OneStepMonoIla() const = 0;
+#endif
+
+  inline z3::context& ctx() const { return ctx_; }
+
+private:
+  z3::context& ctx_;
+
+  Z3ExprAdapter gen_;
+
+  z3::expr InstrUpdCmpl(const InstrPtr instr, const std::string& prev,
+                        const std::string& next);
+#if 0
+  z3::expr InstrUpdBare(const InstrPtr instr, const std::string& prev,
+                        const std::string& next);
+#endif
+
 }; // class Unroller
 
 } // namespace ila
--- src/verify/unroller.cc
+++ src/verify/unroller.cc
@@ -2,10 +2,41 @@
 /// Source for unrolling ILA execution.
 
 #include "verify/unroller.h"
+#include "util/log.h"
 
 namespace ila {
 
-// TODO
+Unroller::Unroller(z3::context& ctx) : ctx_(ctx), gen_(Z3ExprAdapter(ctx)) {}
+
+Unroller::~Unroller() {}
+
+z3::expr Unroller::InstrUpdCmpl(const InstrPtr instr, const std::string& prev,
+                                const std::string& next) {
+  ILA_NOT_NULL(instr);
+  auto ila = instr->host(); // can only unroll Instr with host ILA
+  ILA_NOT_NULL(ila);
+
+  // update function accumulator
+  auto upd_acc = ctx().bool_val(true);
+  // enumerate through all states in the ILA.
+  auto state_num = ila->state_num();
+  for (size_t i = 0; i != state_num; i++) {
+    auto var_i = ila->state(i);           // state var
+    auto upd_i = instr->GetUpdate(var_i); // next state function
+
+    if (upd_i) { // update function has been defined
+      auto next_var = gen_.GetExpr(var_i, next); // next state var
+      auto next_upd = gen_.GetExpr(upd_i, prev); // next state function
+      auto connect = (next_var == next_upd);     // assert same
+      upd_acc = (connect && upd_acc);            // add to the accumulator
+    } else { // update function not yet defined
+      ILA_DLOG("Unroller") << instr << ": " << var_i << " update not defined.";
+    }
+  }
+
+  upd_acc = upd_acc.simplify();
+  return upd_acc;
+}
 
 } // namespace ila
 
