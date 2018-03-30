4818c8ec200b8a5c66eb3b9886475a0b63fa1763
Bo-Yuan-Huang <byhuang1992@gmail.com>
2018-03-30 14:31:20 -0400

    Unit test helper for generating CRR

--- test/t_eq_check.cc
+++ test/t_eq_check.cc
@@ -1,6 +1,7 @@
 /// \file
 /// Unit test for commutating diagram-based equivalence checking
 
+#include "backend/abs_knob.h"
 #include "backend/unroller.h"
 #include "refinement/eq_check_crr.h"
 #include "unit-include/eq_ilas.h"
@@ -18,12 +19,12 @@ public:
   ~TestEqCheck() {}
   void SetUp() {
     DebugLog::Enable("EqCheck");
-    DebugLog::Enable("Verbose-CheckRefine");
+    DebugLog::Enable("Verbose-CrrEqCheck");
   }
   void TearDown() {
     SetToStdErr(0);
     DebugLog::Disable("EqCheck");
-    DebugLog::Disable("Verbose-CheckRefine");
+    DebugLog::Disable("Verbose-CrrEqCheck");
   }
 
   z3::context c;
@@ -35,9 +36,32 @@ public:
   InstrLvlAbsPtr f2 = ila_gen.GetIlaFlat2("f2");
   // InstrLvlAbsPtr h1_ = ila_gen_.GetIlaHier1("h1");
   // InstrLvlAbsPtr h2_ = ila_gen_.GetIlaHier2("h2");
+
+  RefPtr GetRefine(const InstrLvlAbsPtr top, const int& instr_idx, bool comp,
+                   bool flat);
+  RelPtr GetRelation(const InstrLvlAbsPtr m1, const InstrLvlAbsPtr m2);
 };
 
-TEST_F(TestEqCheck, CommDiag_FF) {
+TEST_F(TestEqCheck, FF_Mono) {
+  SetToStdErr(1);
+  DebugLog::Disable("Verbose-CrrEqCheck");
+
+  // refinement
+  auto instr_idx = 0;
+  auto ref1 = GetRefine(f1, instr_idx, false, true);
+  auto ref2 = GetRefine(f2, instr_idx, false, true);
+
+  // relation
+  auto rel = GetRelation(f1, f2);
+  // crr
+  auto crr = CompRefRel::New(ref1, ref2, rel);
+  auto cd = CommDiag(c, crr);
+
+  auto res = cd.EqCheck();
+  EXPECT_TRUE(res);
+}
+
+TEST_F(TestEqCheck, CommDiag_FF_Legacy) {
   SetToStdErr(1);
 
   // 1. coi analysis XXX
@@ -79,7 +103,7 @@ TEST_F(TestEqCheck, CommDiag_FF) {
     ref1->set_appl(f1->instr(instr_idx)->decode());
     ref2->set_appl(f2->instr(instr_idx)->decode());
 
-    for (auto i = 0; i != 1; i++) {
+    for (auto i = 0; i != 2; i++) {
       auto vc = cd.GenVerCond(i);
       s.reset();
       s.add(vc);
@@ -132,6 +156,54 @@ TEST_F(TestEqCheck, CommDiag_Pipeline) {
   ExmpStrmBuff sb;
   auto m = sb.GetStrmBuffSpecRaw("spec");
   // TODO
+}
+
+RefPtr TestEqCheck::GetRefine(const InstrLvlAbsPtr top, const int& instr_idx,
+                              bool comp, bool flat) {
+  auto ref = RefinementMap::New();
+  // target
+  if (comp) { // compositional
+    ref->set_tgt(top->instr(instr_idx));
+  } else { // monolithic
+    ref->set_tgt(top);
+  }
+  // apply
+  ref->set_appl(top->instr(instr_idx)->decode());
+  // flush
+  ref->set_flush(Not(top->input("start")));
+  // complete
+  std::set<InstrPtr> instrs;
+  AbsKnob::GetInstrOfIla(top, instrs);
+  auto cmpl = BoolConst(true);
+  for (auto it = instrs.begin(); it != instrs.end(); it++) {
+    auto instr = *it;
+    cmpl = And(cmpl, Not(instr->decode()));
+  }
+  ref->set_cmpl(cmpl);
+  // step
+  if (flat) {
+    ref->set_step_orig(0);
+    ref->set_step_appl(0);
+  }
+  // invariant
+  ref->add_inv(Ule(f1->state("counter"), 7));
+
+  return ref;
+}
+
+RelPtr TestEqCheck::GetRelation(const InstrLvlAbsPtr m1,
+                                const InstrLvlAbsPtr m2) {
+  auto rel = RelationMap::New();
+  for (decltype(f1->state_num()) i = 0; i != f1->state_num(); i++) {
+    auto var1 = f1->state(i);
+    try {
+      auto var2 = f2->state(var1->name().str());
+      rel->add(Eq(var1, var2));
+    } catch (...) {
+      ILA_DLOG("EqCheck") << "Manual relation mapping required.";
+    }
+  }
+  return rel;
 }
 
 } // namespace ila
