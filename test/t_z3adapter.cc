6a2e59109104e856888ff36b6afdc57bb1ccbbc1
zhanghongce <zhanghongce@126.com>
2018-10-20 22:27:16 -0400

    add z3adapter suffix test

--- test/t_z3adapter.cc
+++ test/t_z3adapter.cc
@@ -50,9 +50,34 @@ TEST(TestZ3Adapter, Construct) {
   DebugLog::Disable("z3_adapter");
 }
 
+TEST(TestZ3Adapter, Suffix) {
+
+  DebugLog::Enable("z3_adapter");
+
+  z3::context c;
+  z3::solver s(c);
+  Z3ExprAdapter adapter(c);
+
+  auto reg_x = ExprFuse::NewBvVar("reg_x", 8);
+  auto reg_y = ExprFuse::NewBvVar("reg_y", 8);
+
+  z3::expr expr_x_plus_y_frame_1_ = adapter.GetExpr(ExprFuse::Add(reg_x,reg_y), "_frame_1_");
+  z3::expr expr_x_plus_y_frame_2_ = adapter.GetExpr(ExprFuse::Add(reg_x,reg_y), "_frame_2_");
+
+  z3::expr two_frames_should_be_independent = expr_x_plus_y_frame_1_ != expr_x_plus_y_frame_2_;
+
+  s.add(two_frames_should_be_independent);
+
+  auto result = s.check();
+  EXPECT_TRUE(result == z3::sat);
+
+  DebugLog::Disable("z3_adapter");
+}
+
 // TODO simplify
 // TODO change context
 // TODO Clear
+
 
 } // namespace ila
 
