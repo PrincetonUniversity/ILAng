fd3e4f925d66d7d48e98467140bad6b4dd1c9342
Bo-Yuan Huang <byhuang1992@gmail.com>
2018-11-03 16:15:59 -0400

    Add test files for integrating 3-rd party parser

---
+++ include/translator/verilog_parse.h
@@ -0,0 +1,16 @@
+/// \file
+/// Header for testing Verilog files
+
+#ifndef VERILOG_PARSE_H__
+#define VERILOG_PARSE_H__
+
+#include "verilog-parser/src/verilog_parser.h"
+
+/// \namespace ila
+namespace ila {
+
+void TestParseVerilog();
+
+}; // namespace ila
+
+#endif // VERILOG_PARSE_H__
---
+++ src/translator/verilog_parse.cc
@@ -0,0 +1,13 @@
+/// \file
+/// Test for using third-party verilog-parse
+
+#include "translator/verilog_parse.h"
+
+namespace ila {
+
+void TestParseVerilog() {
+  verilog_parser_init();
+  // TODO
+}
+
+}; // namespace ila
