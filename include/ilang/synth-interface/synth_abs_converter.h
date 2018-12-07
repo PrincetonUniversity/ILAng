bfdb156ce4f144cea1e199df5f0e598fc4a26459
Bo-Yuan Huang <byhuang1992@gmail.com>
2018-12-07 17:14:07 +0000

    Node to expr conversion clean up

--- include/ilang/synth-interface/synth_abs_converter.h
+++ include/ilang/synth-interface/synth_abs_converter.h
@@ -42,7 +42,7 @@ public:
   /// \param[in] node the node pointer from the synthesis engine.
   /// \param[in] ila the host ILA model.
   /// \return pointer to the converted expression.
-  ExprPtr ConvertSynthNodeToIlangExpr(const ilasynth::Node* node,
+  ExprPtr ConvertSynthNodeToIlangExpr(const ilasynth::nptr_t& node,
                                       const InstrLvlAbsPtr& ila);
 
 private:
@@ -60,37 +60,38 @@ private:
   std::map<const ilasynth::Node*, ExprPtr> node_expr_map_;
 
   // ------------------------- HELPERS -------------------------------------- //
+
+  // ------------------------- METHODS -------------------------------------- //
   /// Define input variables in ila based on abs.
-  bool PortInputs(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
+  void PortInputs(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
   /// Define state variables in ila based on abs.
-  bool PortStates(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
+  void PortStates(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
   /// Define the valid function in ila based on abs.
-  bool PortValid(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
+  void PortValid(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
   /// Define the fetch function in ila based on abs.
-  bool PortFetch(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
+  void PortFetch(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
   /// Define initial condition in ila based on abs.
-  bool PortInits(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
+  void PortInits(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
 
   /// Collect uninterpreted functions from abs.
-  bool PortFuncs(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
+  void PortFuncs(const ilasynth::Abstraction& abs, const InstrLvlAbsPtr& ila);
   /// Collect decode functions from abs.
-  bool PortDecodeFuncs(const ilasynth::Abstraction& abs,
+  void PortDecodeFuncs(const ilasynth::Abstraction& abs,
                        const InstrLvlAbsPtr& ila);
   /// Collect next state functions from abs.
-  bool PortNextStateFuncs(const ilasynth::Abstraction& abs,
+  void PortNextStateFuncs(const ilasynth::Abstraction& abs,
                           const InstrLvlAbsPtr& ila);
 
-  /// Convert a constant node from the synthesis engine to an expression in ILA.
-  bool ConvertConstNode(const ilasynth::Node* n);
-  /// Convert an op node from the synthesis engine to an expression in ILA.
-  bool ConvertOpNode(const ilasynth::Node* n);
-
-  /// Convert Bool operator from synthesis engine node to ILA expression.
-  ExprPtr ConvertBoolOpNode(const ilasynth::Node* n);
-  /// Convert bit-vector operator from synthesis engine node to ILA expression.
-  ExprPtr ConvertBvOpNode(const ilasynth::Node* n);
-  /// Convert memory operator from synthesis engine node to ILA expression.
-  ExprPtr ConvertMemOpNode(const ilasynth::Node* n);
+  /// The entry point for converting non-var nodes into expr.
+  void CnvtNodeToExpr(const ilasynth::Node* n);
+  /// Convert constant node into expr.
+  void CnvtNodeToExprConst(const ilasynth::Node* n);
+  /// Convert Boolean type operations into expr.
+  void CnvtNodeToExprBoolOp(const ilasynth::Node* n);
+  /// Convert Bit-vector type operations into expr.
+  void CnvtNodeToExprBvOp(const ilasynth::Node* n);
+  /// Convert Memory (array) type operations into expr.
+  void CnvtNodeToExprMemOp(const ilasynth::Node* n);
 
 }; // class SynthAbsConverter
 
--- src/synth-interface/synth_abs_converter.cc
+++ src/synth-interface/synth_abs_converter.cc
@@ -20,23 +20,47 @@ InstrLvlAbsPtr SynthAbsConverter::Convert(const ilasynth::Abstraction& abs) {
   auto m = InstrLvlAbs::New(model_name);
 
   // port basic abstraction definition
-  ILA_ERROR_IF(!PortInputs(abs, m)) << "Fail porting input variables";
-  ILA_ERROR_IF(!PortStates(abs, m)) << "Fail porting state variables";
-  ILA_ERROR_IF(!PortValid(abs, m)) << "Fail porting the valid function";
-  ILA_ERROR_IF(!PortFetch(abs, m)) << "Fail porting the fetch function";
-  ILA_ERROR_IF(!PortInits(abs, m)) << "Fail porting initial conditions";
+  PortInputs(abs, m);
+  PortStates(abs, m);
+  PortFuncs(abs, m);
 
-  // collect uninterpreted functions
-  ILA_ERROR_IF(!PortFuncs(abs, m)) << "Fail porting uninterpreted functions";
+  PortValid(abs, m);
+  PortFetch(abs, m);
 
-  // instruction
+  PortInits(abs, m);
+
+  // TODO instruction
 
   // TODO child-ILAs
 
   return m;
 }
 
-bool SynthAbsConverter::PortInputs(const ilasynth::Abstraction& abs,
+ExprPtr
+SynthAbsConverter::ConvertSynthNodeToIlangExpr(const ilasynth::nptr_t& node,
+                                               const InstrLvlAbsPtr& ila) {
+  ILA_CHECK(!node_expr_map_.empty()) << "Empty map -- missing leaf vars";
+  ILA_NOT_NULL(node);
+  auto n = node.get();
+
+  // return if already visit
+  auto pos = node_expr_map_.find(n);
+  if (pos != node_expr_map_.end()) {
+    return pos->second;
+  }
+
+  // create new Expr w.r.t the node
+  auto CnvtNode2Expr = [this](const ilasynth::Node* nl) { CnvtNodeToExpr(nl); };
+
+  node->depthFirstVisit(CnvtNode2Expr);
+
+  pos = node_expr_map_.find(n);
+  ILA_ASSERT(pos != node_expr_map_.end()) << "Fail converting " << n->getName();
+
+  return pos->second;
+}
+
+void SynthAbsConverter::PortInputs(const ilasynth::Abstraction& abs,
                                    const InstrLvlAbsPtr& ila) {
   auto inps_synth = abs.getInps();
   for (auto it : inps_synth) {
@@ -45,37 +69,42 @@ bool SynthAbsConverter::PortInputs(const ilasynth::Abstraction& abs,
     auto name = node->getName();
     ILA_WARN_IF(name != it.first) << name << " != " << it.first;
 
+    // create input var accordingly
     if (type.isBool()) {
       ila->NewBoolInput(name);
     } else if (type.isBitvector()) {
       ila->NewBvInput(name, type.bitWidth);
     } else {
-      ILA_CHECK(type.isMem()) << "Unknown node type " << type;
+      ILA_ASSERT(type.isMem())
+          << "Unknown type " << type << " for input " << name;
       ila->NewMemInput(name, type.addrWidth, type.dataWidth);
     }
 
+    // update book keeping
+    ILA_ASSERT(node_expr_map_.find(node.get()) == node_expr_map_.end());
     node_expr_map_[node.get()] = ila->input(name);
+
     ILA_DLOG("SynthImport") << "Input: " << ila->input(name);
   }
-
-  return true;
 }
 
-bool SynthAbsConverter::PortStates(const ilasynth::Abstraction& abs,
+void SynthAbsConverter::PortStates(const ilasynth::Abstraction& abs,
                                    const InstrLvlAbsPtr& ila) {
   // bool
   auto bits_synth = abs.getBits();
   for (auto it : bits_synth) {
     auto node = it.second.var;
-
     auto type = node->getType();
-    ILA_CHECK(type.isBool());
-
     auto name = node->getName();
+
+    ILA_ASSERT(type.isBool());
     ILA_WARN_IF(name != it.first) << name << " != " << it.first;
 
     auto var = ila->NewBoolState(name);
+
+    ILA_ASSERT(node_expr_map_.find(node.get()) == node_expr_map_.end());
     node_expr_map_[node.get()] = var;
+
     ILA_DLOG("SynthImport") << "Bool Var: " << ila->state(name);
   }
 
@@ -83,15 +112,17 @@ bool SynthAbsConverter::PortStates(const ilasynth::Abstraction& abs,
   auto regs_synth = abs.getRegs();
   for (auto it : regs_synth) {
     auto node = it.second.var;
-
     auto type = node->getType();
-    ILA_CHECK(type.isBitvector());
-
     auto name = node->getName();
+
+    ILA_ASSERT(type.isBitvector());
     ILA_WARN_IF(name != it.first) << name << " != " << it.first;
 
     auto var = ila->NewBvState(name, type.bitWidth);
+
+    ILA_ASSERT(node_expr_map_.find(node.get()) == node_expr_map_.end());
     node_expr_map_[node.get()] = var;
+
     ILA_DLOG("SynthImport") << "Bv Var: " << ila->state(name);
   }
 
@@ -99,76 +130,45 @@ bool SynthAbsConverter::PortStates(const ilasynth::Abstraction& abs,
   auto mems_synth = abs.getMems();
   for (auto it : mems_synth) {
     auto node = it.second.var;
-
     auto type = node->getType();
-    ILA_CHECK(type.isMem());
-
     auto name = node->getName();
+
+    ILA_ASSERT(type.isMem());
     ILA_WARN_IF(name != it.first) << name << " != " << it.first;
 
     auto var = ila->NewMemState(name, type.addrWidth, type.dataWidth);
+
+    ILA_ASSERT(node_expr_map_.find(node.get()) == node_expr_map_.end());
     node_expr_map_[node.get()] = var;
-    ILA_DLOG("SynthImport") << "Mem: " << ila->state(name);
+
+    ILA_DLOG("SynthImport") << "Mem Var: " << ila->state(name);
   }
-
-  // TODO what strategy?
-
-  return true;
 }
 
-ExprPtr
-SynthAbsConverter::ConvertSynthNodeToIlangExpr(const ilasynth::Node* node,
-                                               const InstrLvlAbsPtr& ila) {
-  ILA_NOT_NULL(node);
-
-  // return if already visit
-  auto pos = node_expr_map_.find(node);
-  if (pos != node_expr_map_.end()) {
-    return pos->second;
-  }
-
-  // create new Expr w.r.t the node
-  auto CnvtNode2Expr = [this](const ilasynth::Node* n) {
-    if (n->isConstant()) {
-      ILA_CHECK(ConvertConstNode(n));
-    } else {
-      ILA_CHECK(ConvertOpNode(n));
-    }
-  };
-
-  node->depthFirstVisit(CnvtNode2Expr);
-
-  pos = node_expr_map_.find(node);
-  ILA_CHECK(pos != node_expr_map_.end())
-      << "Fail converting " << node->getName();
-
-  return pos->second;
-}
-
-bool SynthAbsConverter::PortValid(const ilasynth::Abstraction& abs,
+void SynthAbsConverter::PortValid(const ilasynth::Abstraction& abs,
                                   const InstrLvlAbsPtr& ila) {
   auto valid_synth = abs.getFetchValidNode();
   auto type = valid_synth->getType();
   ILA_WARN_IF(!type.isBool()) << "Non-Bool valid function " << valid_synth;
 
-  auto valid_ilang = ConvertSynthNodeToIlangExpr(valid_synth.get(), ila);
+  auto valid_ilang = ConvertSynthNodeToIlangExpr(valid_synth, ila);
 
-  return true;
+  return;
 }
 
-bool SynthAbsConverter::PortFetch(const ilasynth::Abstraction& abs,
+void SynthAbsConverter::PortFetch(const ilasynth::Abstraction& abs,
                                   const InstrLvlAbsPtr& ila) {
   // TODO
-  return true;
+  return;
 }
 
-bool SynthAbsConverter::PortInits(const ilasynth::Abstraction& abs,
+void SynthAbsConverter::PortInits(const ilasynth::Abstraction& abs,
                                   const InstrLvlAbsPtr& ila) {
   // TODO
-  return true;
+  return;
 }
 
-bool SynthAbsConverter::PortFuncs(const ilasynth::Abstraction& abs,
+void SynthAbsConverter::PortFuncs(const ilasynth::Abstraction& abs,
                                   const InstrLvlAbsPtr& ila) {
   auto funs_synth = abs.getFuns();
   for (auto it : funs_synth) {
@@ -201,29 +201,49 @@ bool SynthAbsConverter::PortFuncs(const ilasynth::Abstraction& abs,
     funcs_[name] = func;
   }
 
-  return true;
+  return;
 }
 
-bool SynthAbsConverter::PortDecodeFuncs(const ilasynth::Abstraction& abs,
+void SynthAbsConverter::PortDecodeFuncs(const ilasynth::Abstraction& abs,
                                         const InstrLvlAbsPtr& ila) {
   // TODO
-  return true;
+  return;
 }
 
-bool SynthAbsConverter::PortNextStateFuncs(const ilasynth::Abstraction& abs,
+void SynthAbsConverter::PortNextStateFuncs(const ilasynth::Abstraction& abs,
                                            const InstrLvlAbsPtr& ila) {
   // TODO
-  return true;
+  return;
 }
 
-bool SynthAbsConverter::ConvertConstNode(const ilasynth::Node* n) {
-  // var should be visit before
-  ILA_CHECK(n->isConstant()) << "Non-constant leaf node " << n->getName();
-  ILA_CHECK(node_expr_map_.find(n) == node_expr_map_.end());
+void SynthAbsConverter::CnvtNodeToExpr(const ilasynth::Node* n) {
+  ILA_NOT_NULL(n);
+  ILA_ASSERT(node_expr_map_.find(n) == node_expr_map_.end());
 
-  // create constant accordingly
-  auto type = n->getType();
+  // Only convert consts and operators, since we need host info for vars.
+  // vars should and must be already created.
+  if (n->isConstant()) {
+    CnvtNodeToExprConst(n);
+  } else {
+    auto type = n->getType();
+    if (type.isBool()) {
+      CnvtNodeToExprBoolOp(n);
+    } else if (type.isBitvector()) {
+      CnvtNodeToExprBvOp(n);
+    } else {
+      ILA_ASSERT(type.isMem()) << "Unknown type for operation " << type;
+      CnvtNodeToExprMemOp(n);
+    }
+  }
+
+  return;
+}
+
+void SynthAbsConverter::CnvtNodeToExprConst(const ilasynth::Node* n) {
+  // place holder for the result
   decltype(ExprFuse::BoolConst(true)) expr = NULL;
+
+  auto type = n->getType();
   if (type.isBool()) {
     auto val = true; // FIXME
     expr = ExprFuse::BoolConst(val);
@@ -231,50 +251,24 @@ bool SynthAbsConverter::ConvertConstNode(const ilasynth::Node* n) {
     auto val = 0; // FIXME
     expr = ExprFuse::BvConst(val, type.bitWidth);
   } else {
-    ILA_CHECK(type.isMem()) << "Unknown constant type " << type;
-    auto def_val = 0; // FIXME also the entry data
+    ILA_ASSERT(type.isMem());
+    auto def_val = 0; // FIXME
     expr = ExprFuse::MemConst(def_val, type.addrWidth, type.dataWidth);
   }
 
-  ILA_NOT_NULL(expr) << "Constant node not converted";
+  ILA_NOT_NULL(expr) << "Fail converting constant node " << n->getName();
   node_expr_map_[n] = expr;
 
-  return true;
+  return;
 }
 
-bool SynthAbsConverter::ConvertOpNode(const ilasynth::Node* n) {
-  ILA_CHECK(node_expr_map_.find(n) == node_expr_map_.end());
-
-  auto type = n->getType();
-  // decltype(ExprFuse::BoolConst(true)) expr = NULL;
-
-  if (type.isBool()) {
-    auto expr = ConvertBoolOpNode(n);
-    node_expr_map_[n] = expr;
-  } else if (type.isBitvector()) {
-    auto expr = ConvertBvOpNode(n);
-    node_expr_map_[n] = expr;
-  } else {
-    ILA_CHECK(type.isMem()) << "Unknown constant type " << type;
-    auto expr = ConvertMemOpNode(n);
-    node_expr_map_[n] = expr;
-  }
-
-  // ILA_NOT_NULL(expr);
-  // node_expr_map_[n] = expr;
-
-  return true;
-}
-
-ExprPtr SynthAbsConverter::ConvertBoolOpNode(const ilasynth::Node* n) {
+void SynthAbsConverter::CnvtNodeToExprBoolOp(const ilasynth::Node* n) {
   // get input arguments
   ExprPtrVec expr_args = {};
   for (auto i = 0; i != n->nArgs(); i++) {
     auto node_arg_i = n->arg(i);
-    ILA_NOT_NULL(node_arg_i) << "NULL operand";
-
     auto pos = node_expr_map_.find(node_arg_i.get());
-    ILA_CHECK(pos != node_expr_map_.end()) << "Missed DF-visit";
+    ILA_ASSERT(pos != node_expr_map_.end()) << "Invalid DF-visit";
 
     auto expr_arg_i = pos->second;
     expr_args.push_back(expr_arg_i);
@@ -345,32 +339,59 @@ ExprPtr SynthAbsConverter::ConvertBoolOpNode(const ilasynth::Node* n) {
     expr = ExprFuse::Eq(expr_args.at(0), expr_args.at(1));
     break;
   case ilasynth::BoolOp::Op::DISTINCT:
-    // XXX check what is distinct
     expr = ExprFuse::Ne(expr_args.at(0), expr_args.at(1));
     break;
   case ilasynth::BoolOp::Op::IF:
     expr = ExprFuse::Ite(expr_args.at(0), expr_args.at(1), expr_args.at(2));
     break;
   default:
-    ILA_ERROR << "Cannot find matched Bool Op for " << n->getName();
+    ILA_ERROR << "Cannot find corresponding Bool Op for " << n->getName();
     break;
   };
 
   ILA_NOT_NULL(expr) << "Fail converting Bool Op node " << n->getName();
-  return expr;
+  node_expr_map_[n] = expr;
+
+  return;
 }
 
-ExprPtr SynthAbsConverter::ConvertBvOpNode(const ilasynth::Node* n) {
-  ILA_NOT_NULL(n);
-  ILA_DLOG("Synthimport") << "Convert Bv op node";
-  // TODO
-  return NULL;
+void SynthAbsConverter::CnvtNodeToExprBvOp(const ilasynth::Node* n) {
+  // get input arguments
+  ExprPtrVec expr_args = {};
+  for (auto i = 0; i != n->nArgs(); i++) {
+    auto node_arg_i = n->arg(i);
+    auto pos = node_expr_map_.find(node_arg_i.get());
+    ILA_ASSERT(pos != node_expr_map_.end()) << "Invalid DF-visit";
+
+    auto expr_arg_i = pos->second;
+    expr_args.push_back(expr_arg_i);
+  }
+
+  // get input parameters
+
+  // construct Expr
+  auto op_ptr = dynamic_cast<const ilasynth::BitvectorOp*>(n);
+  ILA_NOT_NULL(op_ptr) << "Fail casting " << n->getName() << " to Bv Op";
+
+  decltype(ExprFuse::BvConst(1, 1)) expr = NULL;
+
+  switch (op_ptr->getOp()) {
+  case (ilasynth::BitvectorOp::Op::NEGATE):
+    // expr = ExprFuse::Not(expr_args.at(0));
+  default:
+    ILA_ERROR << "Cannot find corresponding Bv Op for " << n->getName();
+    break;
+  };
+
+  ILA_NOT_NULL(expr) << "Fail converting Bv Op node " << n->getName();
+  node_expr_map_[n] = expr;
+
+  return;
 }
 
-ExprPtr SynthAbsConverter::ConvertMemOpNode(const ilasynth::Node* n) {
-  ILA_NOT_NULL(n);
+void SynthAbsConverter::CnvtNodeToExprMemOp(const ilasynth::Node* n) {
   // TODO
-  return NULL;
+  return;
 }
 
 } // namespace ilang
