23137e934111573f1a08b1fa88afddd4e58289f9
Bo-Yuan Huang <byhuang1992@gmail.com>
2018-12-09 15:46:36 -0500

    Use STL style bound check

--- test/t_expr.cc
+++ test/t_expr.cc
@@ -1,20 +1,20 @@
 /// \file
 /// Unit test for Expr
 
-#include <ilang/ila/hash_ast.h>
 #include "unit-include/expr_bank.h"
+#include <ilang/ila/hash_ast.h>
 
 namespace ilang {
 
 using namespace ExprFuse;
 
 class TestExpr : public ::testing::Test, public ExprBank<8> {
- public:
+public:
   TestExpr() {}
   ~TestExpr() {}
   void SetUp() {}
   void TearDown() {}
-};  // class TestExpr
+}; // class TestExpr
 
 TEST_F(TestExpr, ReplaceArgIdx) {
   auto a = And(BOOL[0], BOOL[1]);
@@ -43,7 +43,12 @@ TEST_F(TestExpr, ReplaceArgExpr) {
   EXPECT_NE(c, e);
   EXPECT_TRUE(TopEq(c, e));
 
-  EXPECT_DEATH(c->replace_arg(b, d), ".*");
+  std::string msg = "";
+  GET_STDERR_MSG(c->replace_arg(b, d), msg);
+  EXPECT_TRUE(TopEq(c, e));
+#ifndef NDEBUG
+  EXPECT_FALSE(msg.empty());
+#endif
 }
 
-}  // namespace ilang
+} // namespace ilang
