8f1d0b7db672cb387f7da82c0b44ab91513fa044
Bo-Yuan Huang <byhuang1992@gmail.com>
2018-12-10 16:08:58 -0500

    Document synthesis engine interface and follow naming convention

--- test/t_synth_import.cc
+++ test/t_synth_import.cc
@@ -1,5 +1,5 @@
 /// \file
-/// Unit test for importing ILA from the synthesis engine
+/// Unit tests for importing ILA from the synthesis engine
 
 #include "unit-include/config.h"
 #include "unit-include/util.h"
@@ -11,8 +11,8 @@ TEST(TestSynthImport, AES) {
   SetToStdErr(1);
   // DebugLog::Enable("SynthImport");
 
-  auto fileName = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/all";
-  auto m = ImportSynthAbsFromFile(fileName, "AES");
+  auto file_name = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/all";
+  auto m = ImportSynthAbsFromFile(file_name, "AES");
 
   DebugLog::Disable("SynthImport");
   SetToStdErr(0);
@@ -22,8 +22,8 @@ TEST(TestSynthImport, microAES) {
   SetToStdErr(1);
   // DebugLog::Enable("SynthImport");
 
-  auto fileName = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/allu";
-  auto m = ImportSynthAbsFromFile(fileName, "AES_U");
+  auto file_name = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/allu";
+  auto m = ImportSynthAbsFromFile(file_name, "AES_U");
 
   DebugLog::Disable("SynthImport");
   SetToStdErr(0);
@@ -33,10 +33,10 @@ TEST(TestSynthImport, AddChild) {
   SetToStdErr(1);
   // DebugLog::Enable("SynthImport");
 
-  auto aesFile = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/all";
-  auto aesuFile = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/allu";
-  auto aes = ImportSynthAbsFromFile(aesFile, "AES");
-  auto aesu = ImportSynthAbsFromFile(aesuFile, "AES_U");
+  auto aes_file = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/all";
+  auto aesu_file = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/allu";
+  auto aes = ImportSynthAbsFromFile(aes_file, "AES");
+  auto aesu = ImportSynthAbsFromFile(aesu_file, "AES_U");
 
   aes->AddChild(aesu);
   // aes->MergeChild();
@@ -49,11 +49,11 @@ TEST(TestSynthImport, PortChild) {
   SetToStdErr(1);
   // DebugLog::Enable("SynthImport");
 
-  auto aesFile = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/all";
-  auto aesuFile = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/allu";
+  auto aes_file = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/all";
+  auto aesu_file = std::string(TEST_CONFIG_TEST_ROOT) + "/unit-data/aes_v/allu";
 
-  auto aes_ila = ImportSynthAbsFromFile(aesFile, "AES");
-  auto aesu_ila = ImportSynthAbsFromFileHier(aesuFile, aes_ila, "AES_U");
+  auto aes_ila = ImportSynthAbsFromFile(aes_file, "AES");
+  auto aesu_ila = ImportSynthAbsFromFileHier(aesu_file, aes_ila, "AES_U");
 
   DebugLog::Disable("SynthImport");
   SetToStdErr(0);
