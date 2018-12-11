3cc235a30c10c3c04a837472f04621e7270c1018
Bo-Yuan Huang <byhuang1992@gmail.com>
2018-12-11 18:15:31 -0500

    Add bison and boost in brew list and fix typo

--- scripts/submit-coverage.sh
+++ scripts/submit-coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-lcov --directory $1 --capture --outputfile ./coverage.info
+lcov --directory $1 --capture --output-file ./coverage.info
 lcov --remove ./coverage.info '*/extern/*' '*/test/*' '/usr/*' --output-file ./coverage.info
 lcov --list ./coverage.info
 coveralls-lcov ./coverage.info 
