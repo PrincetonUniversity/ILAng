56c14354a3ea230de21738e2c1e8a2fac6d1a7f9
Bo-Yuan Huang <byhuang1992@gmail.com>
2018-12-22 21:43:07 +0000

    Create Docker image for dev using ILAng

---
+++ dockers/ilang-dev/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+##
+## before install
+##
+apt update --yes
+apt install --yes flex
+apt install --yes bison
+apt install --yes build-essential
+apt install --yes libboost-all-dev
+apt install --yes cmake
+apt install --yes vim
+DEBIAN_FRONTEND=noninteractive apt install --yes git
+
+export DOCKER_WORK_ROOT=$(pwd)
+export DOCKER_TOOL_ROOT=$DOCKER_WORK_ROOT/packages
+mkdir -p $DOCKER_TOOL_ROOT
+
+##
+## install: z3
+##
+cd $DOCKER_TOOL_ROOT
+git clone https://github.com/Z3Prover/z3.git
+cd z3
+git checkout tags/z3-4.7.1 -b v471
+python scripts/mk_make.py
+cd build
+make
+make install
+z3 --version
+
+##
+## build script
+##
+cd $DOCKER_TOOL_ROOT
+git clone https://github.com/Bo-Yuan-Huang/ILA-Tools.git
+cd ILA-Tools
+mkdir -p build
+cd build
+cmake .. -DILANG_INSTALL_DEV=ON
+make
+make install
+make test
+
+##
+## configure example
+##
+cp -r $DOCKER_TOOL_ROOT/ILA-Tools/examples/simple $DOCKER_WORK_ROOT/example
