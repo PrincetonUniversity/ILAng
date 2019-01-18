97fb01b34d0e323f266442980d0c28c0994bba42
Hongce Zhang <zhanghongce@126.com>
2019-01-17 20:09:06 -0500

    add inv syn test

---
+++ test/t_inv_syn.cc
@@ -0,0 +1,35 @@
+/// \file
+/// Unit test for generating Verilog verification target
+
+#include <ilang/ila/instr_lvl_abs.h>
+#include <ilang/ilang++.h>
+#include <ilang/synth-interface/synth_engine_interface.h>
+#include <ilang/vtarget-out/vtarget_gen.h>
+
+#include "unit-include/config.h"
+#include "unit-include/pipe_ila.h"
+#include "unit-include/memswap.h"
+#include "unit-include/util.h"
+
+namespace ilang {
+
+
+TEST(TestVlgVerifInvSyn, PipeExampleYosys) {
+  auto ila_model = SimplePipe::BuildModel();
+
+  auto dirName = std::string(ILANG_TEST_SRC_ROOT) + "/unit-data/vpipe/";
+  VerilogVerificationTargetGenerator vg(
+      {},                          // no include
+      {dirName + "simple_pipe.v"}, //
+      "pipeline_v",                // top_module_name
+      dirName + "rfmap/vmap.json", // variable mapping
+      dirName + "rfmap/cond-noinv.json", dirName + "verify/", ila_model.get(),
+      VerilogVerificationTargetGenerator::backend_selector::YOSYS);
+
+  EXPECT_FALSE(vg.in_bad_state());
+
+  vg.GenerateTargets();
+
+}
+
+}; // namespace ilang
