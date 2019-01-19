3e6232d618f666c81f987df7086e0f5f12ff853f
Bo-Yuan Huang <byhuang1992@gmail.com>
2019-01-19 07:04:23 +0000

    Update RHEL7 setup script

--- scripts/setup-rhel7.sh
+++ scripts/setup-rhel7.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/bash
 # bash script to set up environment for ILAng development
-export BUILD_ROOT=$(pwd)
-export BUILD_PREFIX=$BUILD_ROOT/ilang-env
-export BUILD_DOWNLOAD=$BUILD_ROOT/ilang-env-dw
+export BUILD_ROOT=$HOME
+export BUILD_PREFIX=/usr/local
+export BUILD_DOWNLOAD=$HOME/packages
 
-virtualenv ilang-env
-source ilang-env/bin/activate
 mkdir -p $BUILD_DOWNLOAD
 
 # build, boost, bison, flex
@@ -16,29 +14,28 @@ cd $BUILD_DOWNLOAD
 wget https://github.com/Kitware/CMake/releases/download/v3.12.4/cmake-3.12.4-Linux-x86_64.tar.gz
 tar zxvf cmake-3.12.4-Linux-x86_64.tar.gz
 cd cmake-3.12.4-Linux-x86_64
-cp -R * $BUILD_PREFIX
+sudo cp -R * $BUILD_PREFIX
 export CMAKD_PREFIX_PATH=$BUILD_PREFIX
 
 # z3
 cd $BUILD_DOWNLOAD
-wget https://github.com/Z3Prover/z3/releases/download/z3-4.7.1/z3-4.7.1.tar.gz
-tar zxvf z3-4.7.1.tar.gz
-cd z3rel
+git clone --depth=1 https://github.com/Z3Prover/z3.git
+cd z3
 python scripts/mk_make.py --prefix=$BUILD_PREFIX
 cd build
 make -j$(nproc)
-make install
+sudo make install
 
 # ILAng
 cd $BUILD_DOWNLOAD
-git clone --depth=1 https://github.com/Bo-Yuan-Huang/ILA-Tools.git
-cd ILA-Tools
+git clone --depth=1 https://github.com/Bo-Yuan-Huang/ILAng.git
+cd ILAng
 git submodule update --init --recursive
 mkdir -p build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX -DILANG_INSTALL_DEV=ON -DILANG_FETCH_DEPS=OFF -DILANG_BUILD_SYNTH=OFF -DILANG_BUILD_TEST=OFF
 make -j$(nproc)
-make install
+sudo make install
 
 # configure simple
-cp -r $BUILD_DOWNLOAD/ILA-Tools/examples/simple/ $BUILD_ROOT/simple/
+cp -r $BUILD_DOWNLOAD/ILAng/examples/simple/ $BUILD_ROOT/simple/
