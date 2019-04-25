b4560df5e59e58fd34d9f8ecfc9e8d986372bcf5
Bo-Yuan Huang <byhuang1992@gmail.com>
2019-04-24 20:10:18 -0400

    Fix typo

--- src/ila/expr_fuse.cc
+++ src/ila/expr_fuse.cc
@@ -297,7 +297,7 @@ ExprPtr ExprFuse::Concat(const ExprPtr hi, const ExprPtr lo) {
   auto const_zero = ExprFuse::BvConst(0x0, 1);
   auto const_one = ExprFuse::BvConst(0x1, 1);
   auto bv_hi = hi->is_bool() ? ExprFuse::Ite(hi, const_one, const_zero) : hi;
-  auto bv_lo = hi->is_bool() ? ExprFuse::Ite(lo, const_one, const_zero) : lo;
+  auto bv_lo = lo->is_bool() ? ExprFuse::Ite(lo, const_one, const_zero) : lo;
   return std::make_shared<ExprOpConcat>(bv_hi, bv_lo);
 }
 
