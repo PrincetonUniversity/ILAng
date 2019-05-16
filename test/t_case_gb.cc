a087f58bdefef4b263dcf34fa2b65db52bdf6cf1
Bo-Yuan Huang <byhuang1992@gmail.com>
2019-05-16 10:40:33 -0400

    Case tests on GB low

--- test/t_case_gb.cc
+++ test/t_case_gb.cc
@@ -4,43 +4,43 @@
 #include "unit-include/config.h"
 #include "unit-include/util.h"
 
+#include <cstdio>
+
 #include <ilang/ilang++.h>
 #include <ilang/util/fs.h>
 
 namespace ilang {
 
-#if 0
-TEST(TestCase, GB_Convert_Export) {
-  SetToStdErr(true);
+TEST(TestCase, GB_Import_Export_Import) {
+  SetToStdErr(false);
 
   // get the abstraction
   auto gb_data_dir = os_portable_append_dir(ILANG_TEST_DATA_DIR, "gb");
 
-  // XXX need to expose the api
   auto gb_abst_top = os_portable_append_dir(gb_data_dir, "gb_top.ila");
-  auto gb_ila = ImportSynthAbsFromFile(gb_abst_top, "gb_top");
+  auto gb_ila = ImportSynthAbstraction(gb_abst_top, "gb_top");
   EXPECT_NE(gb_ila.name(), "");
 
   auto gb_abst_child = os_portable_append_dir(gb_data_dir, "gb_child.ila");
-  auto child = ImportSynthAbsFromFileHier(gb_abst_child, gb_ila, "gb_child");
-  EXPECT_NE(child.name(), "");
+  ImportChildSynthAbstraction(gb_abst_child, gb_ila, "gb_child");
 
   // export
-  IlaSerDesMngr::SerToFile(parent, "gb_low.json"); // FIXME tmp file name
+  auto tmp_file_name = std::tmpnam(NULL);
+  ExportIlaPortable(gb_ila, tmp_file_name);
 
-#if 0
-  EXPECT_GT(gb_ila.state_num(), 0);
-  EXPECT_GT(gb_ila.input_num(), 0);
-  EXPECT_GT(gb_ila.instr_num(), 0);
-  EXPECT_GT(gb_ila.child_num(), 0);
-#endif
+  // import
+  auto read_back = ImportIlaPortable(tmp_file_name);
+
+  EXPECT_EQ(read_back.state_num(), gb_ila.state_num());
+  EXPECT_EQ(read_back.input_num(), gb_ila.input_num());
+  EXPECT_EQ(read_back.instr_num(), gb_ila.instr_num());
+  EXPECT_EQ(read_back.child_num(), gb_ila.child_num());
 
   SetToStdErr(false);
 };
-#endif
 
 TEST(TestCase, GB_Import) {
-  SetToStdErr(true);
+  SetToStdErr(false);
 
   // get the ILA model
   auto gb_data_dir = os_portable_append_dir(ILANG_TEST_DATA_DIR, "gb");
