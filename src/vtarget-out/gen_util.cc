5a2342e0c8f516ee08a6cf80c4969c8ae99c7104
Hongce Zhang <zhanghongce@126.com>
2019-06-08 18:31:15 -0700

    more ignorable wrapper signals

--- src/vtarget-out/gen_util.cc
+++ src/vtarget-out/gen_util.cc
@@ -6,6 +6,7 @@
 
 #include <cmath>
 #include <ilang/ila/expr_fuse.h>
+#include <ilang/util/container_shortcut.h>
 #include <ilang/util/log.h>
 #include <ilang/util/str_util.h>
 #include <ilang/vtarget-out/vtarget_gen_impl.h>
@@ -107,6 +108,21 @@ bool VlgSglTgtGen::TryFindVlgState(const std::string& sname) {
 
 #define SIN(sub, s) (s.find(sub) != std::string::npos)
 
+/// signals generated in the wrapper,
+/// it is normal that you cannot find
+/// them in the verilog
+std::set<std::string> wrapper_signals = {
+  "__START__",
+  "__IEND__",
+  "__ISSUE__",
+  "__STARTED__",
+  "__RESETED__",
+  "__ENDED__",
+  "__ENDFLUSH__",
+  "__FLUSHENDED__",
+  "__CYCLE_CNT__"
+};
+
 // for ila state: add __ILA_SO_
 // for verilog signal: keep as it is should be fine
 // btw, record all referred vlg name
@@ -117,7 +133,7 @@ VlgSglTgtGen::ModifyCondExprAndRecordVlgName(const VarExtractor::token& t) {
   const auto& sname = t.second;
 
   if (token_tp == VarExtractor::token_type::UNKN_S) {
-    ILA_WARN_IF(!(sname == "__IEND__" || sname == "__START__"))
+    ILA_WARN_IF(!IN(sname,wrapper_signals))
         << "In refinement relations: unknown reference to name:" << sname
         << " keep unchanged.";
     return sname;
