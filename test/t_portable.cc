dc706177415154415922aadb3a5bf7c54dc5acdf
Bo-Yuan Huang <byhuang1992@gmail.com>
2019-07-09 00:41:06 -0700

    Disable semantics checking for OC to avoid long testing time

--- test/t_portable.cc
+++ test/t_portable.cc
@@ -13,7 +13,8 @@ namespace ilang {
 
 void Check(Ila& a, Ila& b) { CheckIlaEqLegacy(a.get(), b.get()); }
 
-void SerDes(const std::string& dir, const std::string& file) {
+void SerDes(const std::string& dir, const std::string& file,
+            bool check = true) {
   auto file_dir = os_portable_append_dir(ILANG_TEST_DATA_DIR, dir);
   auto aes_file = os_portable_append_dir(file_dir, file);
   auto ila = ImportIlaPortable(aes_file);
@@ -24,7 +25,9 @@ void SerDes(const std::string& dir, const std::string& file) {
   ExportIlaPortable(ila, tmp_file_name);
   auto des = ImportIlaPortable(tmp_file_name);
 
-  Check(ila, des);
+  if (check) {
+    Check(ila, des);
+  }
 }
 
 TEST(TestPortable, AES_V_TOP) { SerDes("aes", "aes_v_top.json"); }
@@ -43,6 +46,6 @@ TEST(TestPortable, GB_LOW) { SerDes("gb", "gb_low.json"); }
 
 TEST(TestPortable, RBM) { SerDes("rbm", "rbm.json"); }
 
-TEST(TestPortable, OC8051) { SerDes("oc", "oc.json"); }
+TEST(TestPortable, OC8051) { SerDes("oc", "oc.json", false); }
 
 } // namespace ilang
