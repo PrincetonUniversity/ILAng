f90b1e79b2f9455bccd8404d72f318f1a9c5e1f3
yuex1994 <yuex@princeton.edu>
2019-07-24 17:15:25 +0000

    Comments on the enum for default state-update methods to complete ILA model

--- include/ilang/ila/validate_model.h
+++ include/ilang/ila/validate_model.h
@@ -1,11 +1,16 @@
-#ifndef ILANG_UTIL_VALIDATE_MODEL_H__
-#define ILANG_UTIL_VALIDATE_MODEL_H__
+#ifndef ILANG_ILA_VALIDATE_MODEL_H__
+#define ILANG_ILA_VALIDATE_MODEL_H__
 
 #include <ilang/ila/ast_fuse.h>
 #include <ilang/ila/instr_lvl_abs.h>
 #include <ilang/ila/z3_expr_adapter.h>
 
+/// \namespace ilang
 namespace ilang {
+
+/// Unified ID for default state-update methods to auto-complete ILA models.
+/// OLD_VALUE: s_next <- s
+/// NONDET_VALUE: s_next <- nondet_func()
 enum DEFAULT_UPDATE_METHOD { OLD_VALUE = 0, NONDET_VALUE = 1 };
 
 bool CheckDeterminism(const InstrLvlAbsPtr& model_ptr_);
@@ -14,4 +19,4 @@ void CompleteModel(const InstrLvlAbsPtr& model_ptr_,
                     DEFAULT_UPDATE_METHOD dum);
 } // namespace ilang
 
-#endif
+#endif // ILANG_ILA_VALIDATE_MODEL_H__
