dcfa5f64ecb63353cb1a5665715f8653b93bbf0b
yuex1994 <yuex@princeton.edu>
2019-07-24 17:41:38 +0000

    use aes_v.json to test validate_model code

--- test/t_validate_model.cc
+++ test/t_validate_model.cc
@@ -1,32 +1,44 @@
 #include <ilang/ila/validate_model.h>
+#include <ilang/ilang++.h>
+
+#include <ilang/util/fs.h>
 #include "unit-include/config.h"
 #include "unit-include/util.h"
-#include "unit-include/validate_aes_model.h"
 
 namespace ilang {
 
 TEST(TestValidateModel, deterministic_check) {
-  AES tested_model;
-  EXPECT_EQ(CheckDeterminism(tested_model.model.get()), false);
+  auto aes_dir = os_portable_append_dir(ILANG_TEST_DATA_DIR, "aes");
+  auto aes_v_file = os_portable_append_dir(aes_dir, "aes_v.json");
+  auto aes_v = ImportIlaPortable(aes_v_file);
+  EXPECT_EQ(CheckDeterminism(aes_v.get()), true);
 }
 
 TEST(TestValidateModel, completeness) {
-  AES tested_model;
-  EXPECT_EQ(CheckCompleteness(tested_model.model.get()), false);
+  auto aes_dir = os_portable_append_dir(ILANG_TEST_DATA_DIR, "aes");
+  auto aes_v_file = os_portable_append_dir(aes_dir, "aes_v.json");
+  auto aes_v = ImportIlaPortable(aes_v_file);
+  EXPECT_EQ(CheckCompleteness(aes_v.get()), false);
 }
 
 TEST(TestVlidateModel, complete_model_with_old_value) {
-  AES tested_model;
-  EXPECT_EQ(CheckCompleteness(tested_model.model.get()), false);
-  CompleteModel(tested_model.model.get(), DEFAULT_UPDATE_METHOD::OLD_VALUE);
-  EXPECT_EQ(CheckCompleteness(tested_model.model.get()), true);
+  auto aes_dir = os_portable_append_dir(ILANG_TEST_DATA_DIR, "aes");
+  auto aes_v_file = os_portable_append_dir(aes_dir, "aes_v.json");
+  auto aes_v = ImportIlaPortable(aes_v_file);
+ 
+  EXPECT_EQ(CheckCompleteness(aes_v.get()), false);
+  CompleteModel(aes_v.get(), DEFAULT_UPDATE_METHOD::OLD_VALUE);
+  EXPECT_EQ(CheckCompleteness(aes_v.get()), true);
 }
 
 TEST(TestVlidateModel, complete_model_with_nondet_value) {
-  AES tested_model;
-  EXPECT_EQ(CheckCompleteness(tested_model.model.get()), false);
-  CompleteModel(tested_model.model.get(), DEFAULT_UPDATE_METHOD::NONDET_VALUE);
-  EXPECT_EQ(CheckCompleteness(tested_model.model.get()), true);
+  auto aes_dir = os_portable_append_dir(ILANG_TEST_DATA_DIR, "aes");
+  auto aes_v_file = os_portable_append_dir(aes_dir, "aes_v.json");
+  auto aes_v = ImportIlaPortable(aes_v_file);
+ 
+  EXPECT_EQ(CheckCompleteness(aes_v.get()), false);
+  CompleteModel(aes_v.get(), DEFAULT_UPDATE_METHOD::NONDET_VALUE);
+  EXPECT_EQ(CheckCompleteness(aes_v.get()), true);
 }
 
 } // namespace ilang
