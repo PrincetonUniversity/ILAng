7dfe72e1a2b1396e5086a56fd9a14c12f0265196
yuex1994 <yuex@princeton.edu>
2019-07-25 14:59:02 +0000

    add nondet model in unit-test

--- test/t_validate_model.cc
+++ test/t_validate_model.cc
@@ -14,6 +14,13 @@ TEST(TestValidateModel, deterministic_check) {
   EXPECT_EQ(CheckDeterminism(aes_v.get()), true);
 }
 
+TEST(TestValidateModel, nondeterministic_check) {
+  auto aes_dir = os_portable_append_dir(ILANG_TEST_DATA_DIR, "aes");
+  auto aes_nondet_file = os_portable_append_dir(aes_dir, "aes_nondet.json");
+  auto aes_nondet = ImportIlaPortable(aes_nondet_file);
+  EXPECT_EQ(CheckDeterminism(aes_nondet.get()), false);
+}
+
 TEST(TestValidateModel, completeness) {
   auto aes_dir = os_portable_append_dir(ILANG_TEST_DATA_DIR, "aes");
   auto aes_v_file = os_portable_append_dir(aes_dir, "aes_v.json");
@@ -21,7 +28,7 @@ TEST(TestValidateModel, completeness) {
   EXPECT_EQ(CheckCompleteness(aes_v.get()), false);
 }
 
-TEST(TestVlidateModel, complete_model_with_old_value) {
+TEST(TestValidateModel, complete_model_with_old_value) {
   auto aes_dir = os_portable_append_dir(ILANG_TEST_DATA_DIR, "aes");
   auto aes_v_file = os_portable_append_dir(aes_dir, "aes_v.json");
   auto aes_v = ImportIlaPortable(aes_v_file);
@@ -31,7 +38,7 @@ TEST(TestVlidateModel, complete_model_with_old_value) {
   EXPECT_EQ(CheckCompleteness(aes_v.get()), true);
 }
 
-TEST(TestVlidateModel, complete_model_with_nondet_value) {
+TEST(TestValidateModel, complete_model_with_nondet_value) {
   auto aes_dir = os_portable_append_dir(ILANG_TEST_DATA_DIR, "aes");
   auto aes_v_file = os_portable_append_dir(aes_dir, "aes_v.json");
   auto aes_v = ImportIlaPortable(aes_v_file);
