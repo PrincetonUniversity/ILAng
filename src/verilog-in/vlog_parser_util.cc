cab4a46aee6c6a42478ddb1f69e0fe2d4804e4c8
Bo-Yuan Huang <byhuang1992@gmail.com>
2019-09-14 13:13:16 -0700

    Rename coverity flag and FP reducing workaround

--- src/verilog-in/vlog_parser_util.cc
+++ src/verilog-in/vlog_parser_util.cc
@@ -7,16 +7,13 @@
 
 namespace ilang {
 
-#ifdef STATIC_ANALYSIS
-extern void _static_analysis_only_free(char* x);
-#endif
-
 std::string _ast_identifier_tostring(ast_identifier id) {
   auto id_cptr = ast_identifier_tostring(id);
   auto id_bstr = static_cast<std::string>(id_cptr);
 
-#ifdef STATIC_ANALYSIS
-  _static_analysis_only_free(id_cptr);
+#ifdef COVERITY
+  // Used only for static analysis for it cannot identify the free phase
+  free(id_cptr);
 #endif
 
   return id_bstr;
