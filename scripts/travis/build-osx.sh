14cc20d690a00fa97185fe81fb0a77c9e81a7546
Bo-Yuan Huang <byhuang1992@gmail.com>
2019-09-15 23:42:12 -0400

    Update CMake recipe and CI scripts for Boost::python 1.71.0

--- scripts/travis/build-osx.sh
+++ scripts/travis/build-osx.sh
@@ -5,7 +5,7 @@ CI_BUILD_DIR=$1
 cd $CI_BUILD_DIR
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DILANG_BUILD_COV=ON
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DBoost_NO_BOOST_CMAKE=ON
 make -j$(nproc)
 sudo make install
 make test
