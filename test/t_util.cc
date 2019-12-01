415c44b4c28b8c7b7621ddd4fc44a294d87a4dcc
Hongce Zhang <zhanghongce@126.com>
2019-11-30 21:28:51 -0500

    util test

--- test/t_util.cc
+++ test/t_util.cc
@@ -1,6 +1,7 @@
 /// \file
 /// Unit test for utility functions
 
+#include "unit-include/config.h"
 #include "unit-include/util.h"
 #include <ilang/util/fs.h>
 #include <ilang/util/str_util.h>
@@ -34,6 +35,22 @@ void EndRecordLog() {
     EndRecordLog();                                                            \
   } while (0);
 
+
+#if defined(_WIN32) || defined(_WIN64)
+TEST(TestUtil, DirAppend) {
+
+  EXPECT_EQ(os_portable_append_dir("\\a", "b"), "\\a\\b");
+  EXPECT_EQ(os_portable_append_dir("\\a\\", "b"), "\\a\\b");
+  EXPECT_EQ(os_portable_append_dir("a\\", "b"), "a\\b");
+  EXPECT_EQ(os_portable_append_dir("a", "b"), "a\\b");
+  EXPECT_EQ(os_portable_append_dir("a\\", ".\\b"), "a\\.\\b");
+  EXPECT_EQ(os_portable_append_dir("\\a\\", ".\\b"), "\\a\\.\\b");
+
+  EXPECT_ERROR(os_portable_append_dir("a", "\\b"));
+  EXPECT_ERROR(os_portable_append_dir("\\a\\", "\\b"));
+  EXPECT_ERROR(os_portable_append_dir("\\a", "\\b"));
+}
+#else
 TEST(TestUtil, DirAppend) {
 
   EXPECT_EQ(os_portable_append_dir("/a", "b"), "/a/b");
@@ -47,7 +64,19 @@ TEST(TestUtil, DirAppend) {
   EXPECT_ERROR(os_portable_append_dir("/a/", "/b"));
   EXPECT_ERROR(os_portable_append_dir("/a", "/b"));
 }
+#endif
 
+
+#if defined(_WIN32) || defined(_WIN64)
+TEST(TestUtil, FileNameFromDir) {
+
+  EXPECT_EQ(os_portable_file_name_from_path("a"), "a");
+  EXPECT_EQ(os_portable_file_name_from_path("a\\b"), "b");
+  EXPECT_EQ(os_portable_file_name_from_path(".\\a\\b"), "b");
+
+  EXPECT_ERROR(os_portable_file_name_from_path("a\\"));
+}
+#else
 TEST(TestUtil, FileNameFromDir) {
 
   EXPECT_EQ(os_portable_file_name_from_path("a"), "a");
@@ -56,6 +85,85 @@ TEST(TestUtil, FileNameFromDir) {
 
   EXPECT_ERROR(os_portable_file_name_from_path("a/"));
 }
+#endif
+
+#if defined(__unix__) || defined(unix) || defined(__APPLE__) || defined(__MACH__) || defined(__linux__) || defined(__FreeBSD__)
+
+TEST(TestUtil, ExecShell) {
+  auto scriptName = std::string(ILANG_TEST_SRC_ROOT) + "/unit-data/shell_ex/shell.sh";
+  std::vector<std::string> cmd;
+  cmd.push_back("bash");
+  cmd.push_back(scriptName);
+  auto res = os_portable_execute_shell(cmd);
+  EXPECT_EQ(res.failure, execute_result::NONE);
+}
+
+TEST(TestUtil, ExecShellOSPath) {
+  auto scriptName = "ls";
+  std::vector<std::string> cmd;
+  cmd.push_back(scriptName);
+  auto res = os_portable_execute_shell(cmd);
+  EXPECT_EQ(res.failure, execute_result::NONE);
+}
+
+
+TEST(TestUtil, ExecShellRedirect) {
+  auto redirect_file = std::string(ILANG_TEST_SRC_ROOT) + "/unit-data/shell_ex/date_out.txt";
+  auto scriptName = "date";
+  std::vector<std::string> cmd;
+  cmd.push_back(scriptName);
+  execute_result res;
+
+  res = os_portable_execute_shell(cmd,redirect_file, BOTH);
+  EXPECT_EQ(res.failure, execute_result::NONE);
+  
+  std::ifstream d1(redirect_file);
+  std::string l1;
+
+  EXPECT_TRUE(d1.is_open());
+  if (d1.is_open()) {
+    std::getline(d1,l1);
+    d1.close();
+  }
+  
+  sleep(2); // wait for at ~ 2 seconds
+
+  res = os_portable_execute_shell(cmd,redirect_file, BOTH);
+  EXPECT_EQ(res.failure, execute_result::NONE);
+
+  std::ifstream d2(redirect_file);
+  std::string l2;
+
+  EXPECT_TRUE(d2.is_open());
+  if (d2.is_open()) {
+    std::getline(d2,l2);
+    d2.close();
+  }
+
+  EXPECT_TRUE(l1 != l2);
+}
+
+
+TEST(TestUtil, ExecShellRedirectTimeOut) {
+  auto redirect_file = std::string(ILANG_TEST_SRC_ROOT) + "/unit-data/shell_ex/sleep_out.txt";
+  std::vector<std::string> s1cmd({"sleep", "1"});
+  std::vector<std::string> s10cmd({"sleep", "10"});
+  execute_result res;
+
+  res = os_portable_execute_shell(s1cmd,redirect_file, BOTH, 5);
+  EXPECT_TRUE(res.failure == execute_result::NONE);
+  EXPECT_EQ(res.timeout, false);
+  EXPECT_EQ(res.ret, 0);
+
+  res = os_portable_execute_shell(s10cmd,redirect_file, BOTH, 2);
+  EXPECT_EQ(res.failure, execute_result::NONE);
+  EXPECT_EQ(res.timeout, true);
+  // EXPECT_EQ(res.ret, 0); if timeout return value not usable
+
+}
+#endif
+
+
 
 TEST(TestUtil, RegularExpr) {
   if (IsRExprUsable()) {
