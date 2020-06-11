405654f576372d7167abd5a706bc1f229ed88434
Bo-Yuan Huang <byhuang1992@gmail.com>
2020-06-10 22:02:26 -0400

    Clean up log setup in tests

--- test/t_util.cc
+++ test/t_util.cc
@@ -16,24 +16,16 @@ namespace fs = std::experimental::filesystem;
 
 void RecordLog() {
   // precondition for log test
-#if 0
   SetLogLevel(0); // log all
   SetLogPath(""); // log to /tmp
   SetToStdErr(1); // log to stderr for easy catching
-#endif
 }
 
 void EndRecordLog() {
-// reset to default condition
-#if 0
+  // reset to default condition
   SetLogLevel(0); // log all
   SetLogPath(""); // log to /tmp
-#ifndef NDEBUG
-  SetToStdErr(0); // still log to stderr
-#else  // NDEBUG
   SetToStdErr(0); // not log to stderr
-#endif // NDEBUG
-#endif
 }
 
 #define EXPECT_ERROR(m)                                                        \
