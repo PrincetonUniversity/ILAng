b13f5a0878c8a63f47cd16d6a6175649b15faf36
Bo-Yuan Huang <byhuang1992@gmail.com>
2021-10-29 02:29:20 -0400

    Script for setting up smt-switch & update GitHub action

---
+++ scripts/setup-smtswitch.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2019 Princeton University
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+SRC_DIR=$1
+DEP_DIR=$2
+Z3_VERSION=tags/z3-4.8.12
+
+# fetch smt-switch
+cd $SRC_DIR
+git submodule update --init extern/smt-switch
+
+# setup dir for SMT solvers
+mkdir -p $SRC_DIR/extern/smt-switch/deps
+
+# fetch/build/install z3
+cd $SRC_DIR/extern/smt-switch/deps
+git clone https://github.com/Z3Prover/z3.git
+cd z3
+git checkout $Z3_VERSION
+python scripts/mk_make.py --staticlib --single-threaded --prefix=$DEP_DIR
+cd build
+make -j$(nproc)
+make install
+
+#fetch/build boolector
+cd $SRC_DIR/extern/smt-switch
+source contrib/setup-btor.sh
+
+#build/install smt-switch
+mkdir -p $SRC_DIR/extern/smt-switch/build
+cd $SRC_DIR/extern/smt-switch/build
+cmake .. -DBUILD_Z3=ON -DBUILD_BTOR=ON -DCMAKE_INSTALL_PREFIX=$DEP_DIR
+make -j$(nproc)
+make install
